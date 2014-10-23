97195129813857b5afe98ca62ee689c92e5f43f1
Gunther Cox <gunthercx@gmail.com>
2014-10-22 21:16:35 -0400

    Python3 strings use utf-8 encoding.

--- chatterbot/apis/__init__.py
+++ chatterbot/apis/__init__.py
@@ -16,7 +16,7 @@ def clean(text):
     text = text.strip()
 
     # Normalize unicode characters
-    text = str(unicodedata.normalize('NFKD', text).encode('ascii','ignore'))
+    text = unicodedata.normalize('NFKD', text).encode('ascii','ignore').decode("utf-8")
 
     # Replace html characters with ascii equivilant
     text = text.replace("&amp;", "&")
