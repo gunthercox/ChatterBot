ffec9a6982f1e6bdeb36c466f824161bf128244a
Gunther Cox <gunthercx@gmail.com>
2014-11-29 13:51:54 -0500

    Results with equal weight can now be returned. Closes #10.

--- chatterbot/algorithms/engram.py
+++ chatterbot/algorithms/engram.py
@@ -4,9 +4,9 @@ def engram(text, log_directory):
     Returns a response based on the closest match based on in known conversations.
     """
     from chatterbot.conversation import Statement, Conversation
-    import os
+    import os, random
 
-    closest_response = None
+    closest_response = []
     closest_ratio = 0
 
     for log in os.listdir(log_directory):
@@ -18,9 +18,15 @@ def engram(text, log_directory):
             conversation.read(path)
             response, ratio = conversation.find_closest_response(text)
 
-            if ratio > closest_ratio:
-                closest_response = response
-                closest_ratio = ratio
+            # A response may not be returned if a log has less than one line
+            if response:
+                if ratio > closest_ratio:
+                    closest_response = []
+                    closest_response.append(response)
+                    closest_ratio = ratio
+                elif ratio == closest_ratio:
+                    closest_response.append(response)
+                    closest_ratio = ratio
 
     # Seek a better response if the difference ratio is too low or the choice list is empty
     if (not closest_response) or (closest_ratio < 90):
@@ -31,4 +37,4 @@ def engram(text, log_directory):
         default = Statement("Error", "No possible replies could be determined.")
         return [default]
 
-    return closest_response
+    return random.choice(closest_response)
