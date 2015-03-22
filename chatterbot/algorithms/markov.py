32bd5977a51e7ced7f0068006c65c703a8130d51
Gunther Cox <gunthercx@gmail.com>
2015-03-22 15:25:06 -0400

    Removed markov chain placeholder file.

--- chatterbot/algorithms/markov.py
+++
@@ -1,7 +0,0 @@
-def markov(text, log_directory):
-    """
-    Takes a message from a conversation.
-    Returns a responses based on a markov chain algorithm.
-    """
-    # Future
-    pass
--- tests/test_markov.py
+++
@@ -1,8 +0,0 @@
-from .base_case import ChatBotTestCase
-from chatterbot.algorithms.markov import markov
-
-
-class MarkovTests(ChatBotTestCase):
-
-    def test_results(self):
-        pass
