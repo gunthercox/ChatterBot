19b3eb04d905ed6b020d5092baceb4aa0153965f
Gunther Cox <gunthercx@gmail.com>
2015-07-25 06:01:16 -0400

    Removed unused twitter algorithm.

--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -17,20 +17,3 @@ class TwitterTests(TestCase):
         self.assertEqual(TWITTER["CONSUMER_KEY"], chatbot.consumer_key)
         self.assertEqual(TWITTER["CONSUMER_SECRET"], chatbot.consumer_secret)
 
-
-class TwitterAlgorithmTests(TestCase):
-
-    def test_remove_leeding_usernames(self):
-
-        string = "@CERN @Space Happy black friday! Keep hunting for black holes! @NASA"
-        string = remove_leeding_usernames(string)
-        result = "Happy black friday! Keep hunting for black holes! @NASA"
-
-        self.assertEqual(string, result)
-
-    def test_remove_trailing_usernames(self):
-
-        string = "@ASIMO Watch your step @Demo @in @Tokyo"
-        string = remove_trailing_usernames(string)
-
-        self.assertEqual(string, "@ASIMO Watch your step")
