a6ff3a18eceba896b168e942e79cabdf4c0996af
Gunther Cox <gunthercx@gmail.com>
2015-09-12 08:45:10 -0400

    Removed GitHub io adapter.
    This adapter was added as a test a while back and will not work
    with the latest configuration of chatterbot. It is likely that
    there will not be a GitHub io adapter in the future.

--- chatterbot/adapters/io/__init__.py
+++ chatterbot/adapters/io/__init__.py
@@ -1,6 +1,6 @@
 from .io import IOAdapter
 from .terminal import TerminalAdapter
-from .json import JsonAdapter
+from .io_json import JsonAdapter
 from .twitter import TwitterAdapter
-from .github import GitHubAdapter
 from .no_output import NoOutputAdapter
+
