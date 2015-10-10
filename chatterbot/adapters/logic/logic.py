a14f6464d66a2e70112459394af49c6835ad1af6
Gunther Cox <gunthercx@gmail.com>
2015-10-10 14:51:38 -0400

    Added docstrings to adapter interfaces.

--- chatterbot/adapters/logic/logic.py
+++ chatterbot/adapters/logic/logic.py
@@ -2,6 +2,11 @@ from chatterbot.adapters.exceptions import AdapterNotImplementedError
 
 
 class LogicAdapter(object):
+    """
+    This is an abstract class that represents the interface
+    that all logic adapters should implement.
+    """
 
     def get(self, text, list_of_statements, current_conversation=None):
         raise AdapterNotImplementedError()
+
