d2ad4bca173bcfd6ffbd115ef5ca96a57a809f29
Gunther Cox <gunthercx@gmail.com>
2015-10-13 07:19:23 -0400

    Statement object list is now passed to logic adapters.
    This makes it possible to create logic adapters that
    use additional meta data from statements to select
    a response.

--- chatterbot/adapters/logic/closest_match.py
+++ chatterbot/adapters/logic/closest_match.py
@@ -4,20 +4,25 @@ from fuzzywuzzy import process
 
 class ClosestMatchAdapter(LogicAdapter):
 
-    def get(self, text, list_of_statements, current_conversation=None):
+    def get(self, text, statement_list, current_conversation=None):
         """
         Takes a statement string and a list of statement strings.
         Returns the closest matching statement from the list.
         """
 
+        # Get the text of each statement
+        text_of_all_statements = []
+        for statement in statement_list:
+            text_of_all_statements.append(statement.text)
+
         # If the list is empty, return the statement
-        if not list_of_statements:
+        if not text_of_all_statements:
             return text
 
         # Check if an exact match exists
-        if text in list_of_statements:
+        if text in text_of_all_statements:
             return text
 
         # Get the closest matching statement from the database
-        return process.extract(text, list_of_statements, limit=1)[0][0]
+        return process.extract(text, text_of_all_statements, limit=1)[0][0]
 
