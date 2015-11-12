08ede07c4f899c9ff8dbb6f02dc5b49e0dd1436e
Vale <valetolpegin@gmail.com>
2015-11-12 07:43:32 -0500

    Created preprocessor & evaluate_mathematically()

---
+++ chatterbot/adapters/preprocessor/__init__.py
@@ -0,0 +1,1 @@
+from .evaluate_mathematically import EvaluateMathematically
---
+++ chatterbot/adapters/preprocessor/evaluate_mathematically.py
@@ -0,0 +1,77 @@
+from .preprocessor import PreProcessorAdapter
+
+
+class EvaluateMathematically(PreProcessorAdapter):
+
+    def process(self, input_text):
+        """
+        Takes a statement string.
+        Returns the simplified statement string
+        with the mathematical terms "solved".
+        """
+
+        expression = self.simplify_chunks( input_text )
+
+        return input_text + " Test"
+
+
+    def simplify_chunks(self, input_text):
+        """
+        Separates the incoming text.
+        """
+
+        expression = []
+
+        for chunk in input_text.split( ' ' ):
+            is_integer = self.isInteger( chunk )
+
+            if is_integer == False:
+                is_float = self.isFloat( chunk )
+
+                if is_float == False:
+                    continue
+                else:
+                    expression.append( is_float )
+            else:
+                expression.append( is_integer )
+
+
+    def isFloat(self, string):
+        """
+        If the string is a float, returns
+        the float of the string. Otherwise,
+        it returns False.
+        """
+
+        try:
+            float( integer )
+
+            return float( integer );
+        except:
+            return False
+
+
+    def isInteger(self, string):
+        """
+        If the string is an integer, returns
+        the int of the string. Otherwise,
+        it returns False.
+        """
+
+        if string.isdigit():
+            return int( string )
+        else:
+            return False
+
+
+    def isOperator(self, string):
+        """
+        If the string is an operator, returns
+        said operator. Otherwise, it returns
+        false.
+        """
+
+        if string in "+-/*^":
+            return string
+        else:
+            return False
