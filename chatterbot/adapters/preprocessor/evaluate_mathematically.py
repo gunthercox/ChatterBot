382820951a51e55e9b6e524fa62b0a036945e643
Vale <valetolpegin@gmail.com>
2015-11-19 19:03:26 -0500

    Successful evaluation of basic expressions
    The class can now evaluate basic expressions & successfully completes
    tests

--- chatterbot/adapters/preprocessor/evaluate_mathematically.py
+++ chatterbot/adapters/preprocessor/evaluate_mathematically.py
@@ -1,5 +1,6 @@
 from .preprocessor import PreProcessorAdapter
 import re
+import ast
 
 class EvaluateMathematically(PreProcessorAdapter):
 
@@ -10,13 +11,16 @@ class EvaluateMathematically(PreProcessorAdapter):
         with the mathematical terms "solved".
         """
 
-        #input_text = re.sub( '.', '', input_text )
+        # Getting the mathematical terms within the input statement
+        expression, string = self.simplify_chunks( self.normalize( input_text ) )
 
-        expression = self.simplify_chunks( input_text )
+        # Returning important information
+        try:
+            string += '= ' + str( eval( string ) )#self.evaluate( string ) )
 
-        print input_text
-
-        return input_text
+            return string, True
+        except:
+            return string, False
 
 
     def simplify_chunks(self, input_text):
@@ -25,19 +29,44 @@ class EvaluateMathematically(PreProcessorAdapter):
         """
 
         expression = []
+        string = ''
 
         for chunk in input_text.split( ' ' ):
+
             is_integer = self.isInteger( chunk )
 
             if is_integer == False:
                 is_float = self.isFloat( chunk )
 
                 if is_float == False:
-                    continue
+                    is_operator = self.isOperator( chunk )
+
+                    if is_operator == False:
+                        continue
+                    else:
+                        expression.append( is_operator )
+
+                        string += str( is_operator ) + ' '
                 else:
                     expression.append( is_float )
+
+                    string += str( is_float ) + ' '
             else:
                 expression.append( is_integer )
+
+                string += str( is_integer ) + ' '
+
+        return expression, string
+
+
+    def evaluate( self, expression ):
+        """
+        Evaluates a set of expressions
+        and produces an answer. Then,
+        it returns the answer.
+        """
+
+        return eval( expression )
 
 
     def isFloat(self, string):
@@ -79,3 +108,19 @@ class EvaluateMathematically(PreProcessorAdapter):
             return string
         else:
             return False
+
+
+    def normalize(self, string):
+        """
+        Normalizes input text, reducing errors
+        and improper calculations.
+        """
+
+        # Setting all words to lowercase
+        string = string.lower()
+
+        # Removing punctuation
+        string = re.sub( '[.!?/:;]', '', string )
+
+        # Returning normalized text
+        return string
