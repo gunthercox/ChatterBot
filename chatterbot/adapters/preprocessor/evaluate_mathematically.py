778266f0ca0ebc8be7ac887fa8cca1c236585abc
Vale <valetolpegin@gmail.com>
2015-11-22 08:03:48 -0500

    Continued improvement on parser
    Now, it can successfully take any size number that is represented in
    word ( if they are in the table, which is the next thing to improve )

--- chatterbot/adapters/preprocessor/evaluate_mathematically.py
+++ chatterbot/adapters/preprocessor/evaluate_mathematically.py
@@ -135,7 +135,7 @@ class EvaluateMathematically(PreProcessorAdapter):
         """
 
         nums = { "one" : 1, "two" : 2, "three" : 3, "four" : 4 }
-        words = { "and" : '+', "plus" : '+', "minus" : '-', "times" : '*', 'divided by' : '/' }
+        words = { "plus" : '+', "minus" : '-', "times" : '*', 'divided by' : '/' }
         scales = { 'hundred' : '* 100', 'thousand' : '* 1000' }
 
         condensed_string = '_'.join( string.split( ' ' ) )
@@ -159,5 +159,20 @@ class EvaluateMathematically(PreProcessorAdapter):
                 condensed_string[ chunk_index ] = value
             except:
                 pass
+
+        for chunk_index in range( 0, len( condensed_string ) ):
+            if self.isInteger( condensed_string[ chunk_index ] ) or self.isFloat( condensed_string[ chunk_index ] ):
+                i = 1
+                start_index = chunk_index
+                end_index = -1
+                while( chunk_index + i < len( condensed_string ) and ( self.isInteger( condensed_string[ chunk_index + i ] ) or self.isFloat( condensed_string[ chunk_index + i ] ) ) ):
+                    end_index = chunk_index + i
+                    i += 1
+
+                for sub_chunk in range( start_index, end_index ):
+                    condensed_string[ sub_chunk ] += " +"
+
+                condensed_string[ start_index ] = "( " + condensed_string[ start_index ]
+                condensed_string[ end_index ] += " )"
 
         return ' '.join( condensed_string )
