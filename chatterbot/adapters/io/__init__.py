5bfe93cc9aae7ff252390ca38c91315b8127c85c
Gunther Cox <gunthercx@gmail.com>
2016-03-18 19:29:31 -0400

    Remove Mac-only speech synthesis adapter.
    This adapter is being removed from the chatterbot core in favor of
    continuing work with it in the chatterbot-voice repo.
    
    Future work involving speech recognition and speech synthesis will
    continue there (https://github.com/gunthercox/chatterbot-voice).

--- chatterbot/adapters/io/__init__.py
+++ chatterbot/adapters/io/__init__.py
@@ -2,5 +2,4 @@ from .io import IOAdapter
 from .terminal import TerminalAdapter
 from .io_json import JsonAdapter
 from .no_output import NoOutputAdapter
-from .tts import MacOSXTTS
 from .multi_adapter import MultiIOAdapter
