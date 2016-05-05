9e2a298e11e16470c857d15e355cf0b42baafe96
Daniel Heredia <danno@mistercyb.org>
2016-05-04 23:39:24 -0500

    Avoid import chatterbot in __init__.py when the loaded script is setup.py

--- chatterbot/__init__.py
+++ chatterbot/__init__.py
@@ -1,5 +1,12 @@
-from .chatterbot import ChatBot
+import os
+import sys
+
+script_name = os.path.basename(sys.argv[0])
+
+if script_name != 'setup.py':
+        from .chatterbot import ChatBot
 
 __version__ = "0.3.7"
 __author__ = "Gunther Cox"
 __email__ = "gunthercx@gmail.com"
+
