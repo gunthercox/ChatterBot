88b14f4fbf9dad8e4346e4d5489778de66ba6428
Gunther Cox <gunthercx@gmail.com>
2016-05-16 21:06:20 -0400

    Removed old multi-io adapter classes.

--- chatterbot/adapters/input/__init__.py
+++ chatterbot/adapters/input/__init__.py
@@ -1,4 +1,3 @@
 from .input_adapter import InputAdapter
 from .terminal import TerminalAdapter
 from .variable_input_type_adapter import VariableInputTypeAdapter
-from .multi_adapter import MultiInputAdapter
--- chatterbot/adapters/output/__init__.py
+++ chatterbot/adapters/output/__init__.py
@@ -1,5 +1,4 @@
 from .output_adapter import OutputAdapter
 from .terminal import TerminalAdapter
 from .output_format_adapter import OutputFormatAdapter
-from .multi_adapter import MultiOutputAdapter
 from .mailgun import Mailgun
