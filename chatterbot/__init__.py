baaeab26b7440edefe79671da02c763b3c81cbbd
Gunther Cox <gunthercx@gmail.com>
2016-05-24 08:08:23 -0400

    Updated release version.

--- chatterbot/__init__.py
+++ chatterbot/__init__.py
@@ -4,9 +4,9 @@ import sys
 script_name = os.path.basename(sys.argv[0])
 
 if script_name != 'setup.py':
-        from .chatterbot import ChatBot
+    from .chatterbot import ChatBot
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __author__ = "Gunther Cox"
 __email__ = "gunthercx@gmail.com"
 
