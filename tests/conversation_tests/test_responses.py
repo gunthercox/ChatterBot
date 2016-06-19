01cb4425a3b63d72851e1ac40d1d170c3cf423e7
Gunther Cox <gunthercx@gmail.com>
2016-06-19 17:51:42 -0400

    Removed signature objects.

--- tests/conversation_tests/test_responses.py
+++ tests/conversation_tests/test_responses.py
@@ -1,14 +1,9 @@
 from unittest import TestCase
-from chatterbot.conversation import Response, Signature
+from chatterbot.conversation import Response
 
 
 class ResponseTests(TestCase):
 
     def setUp(self):
         self.response = Response("A test response.")
-
-    def test_add_signature(self):
-        signature = Signature("Gunther Cox")
-        self.response.add_signature(signature)
-        self.assertIn(signature, self.response.signatures)
 
