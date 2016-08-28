0bd78ae9d21e73e48406653967044d62cc86eb2d
Gunther Cox <gunthercx@gmail.com>
2016-08-28 19:55:16 -0400

    Create app settings for django chatterbot module.

--- chatterbot/ext/django_chatterbot/apps.py
+++ chatterbot/ext/django_chatterbot/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from chatterbot.ext.django_chatterbot import settings as chatterbot_settings
 
 
-class DjangoChatterBotConfig(AppConfig):
+class DjangoChatterbotConfig(AppConfig):
 
-    name = "chatterbot.ext.django_chatterbot"
+    name = 'chatterbot.ext.django_chatterbot'
+    verbose_name = 'Django ChatterBot'
+
+    def ready(self):
+        chatterbot_settings.patch_all()
--- chatterbot/ext/django_chatterbot/views.py
+++ chatterbot/ext/django_chatterbot/views.py
@@ -1,30 +1,16 @@
 from django.views.generic import View
 from django.http import JsonResponse
-from chatterbot import ChatBot
-from chatterbot.training.trainers import ListTrainer
-
-
-chatterbot = ChatBot(
-    'Example ChatterBot',
-    input_adapter='chatterbot.adapters.input.VariableInputTypeAdapter',
-    output_adapter='chatterbot.adapters.output.OutputFormatAdapter',
-    output_format='json'
-)
-
-chatterbot.set_trainer(ListTrainer)
-
-chatterbot.train([
-    "Hi",
-    "Hello",
-    "How are you?",
-    "I am good.",
-    "That is good to hear.",
-    "Thank you",
-    "You are welcome.",
-])
+from django.conf import settings
 
 
 class ChatterBotView(View):
+
+    def post(self, request, *args, **kwargs):
+        input_statement = request.POST.get('text')
+
+        response_data = settings.CHATTERBOT.get_response(input_statement)
+
+        return JsonResponse(response_data, status=200)
 
     def get(self, request, *args, **kwargs):
         data = {
@@ -34,9 +20,19 @@ class ChatterBotView(View):
         # Return a method not allowed response
         return JsonResponse(data, status=405)
 
-    def post(self, request, *args, **kwargs):
-        input_statement = request.POST.get('text')
+    def patch(self, request, *args, **kwargs):
+        data = {
+            'detail': 'You should make a POST request to this endpoint.'
+        }
 
-        response_data = chatterbot.get_response(input_statement)
+        # Return a method not allowed response
+        return JsonResponse(data, status=405)
 
-        return JsonResponse(response_data, status=200)
+    def delete(self, request, *args, **kwargs):
+        data = {
+            'detail': 'You should make a POST request to this endpoint.'
+        }
+
+        # Return a method not allowed response
+        return JsonResponse(data, status=405)
+
