b832649bc926b49500b36c978ae1b54766adadd0
Gunther Cox <gunthercx@gmail.com>
2016-08-29 13:32:09 -0400

    Do not import when installing.

--- chatterbot/__init__.py
+++ chatterbot/__init__.py
@@ -1,9 +1,7 @@
 import os
 import sys
 
-script_name = os.path.basename(sys.argv[0])
-
-if script_name != 'setup.py' and 'egg_info' not in sys.argv:
+if 'install' not in sys.argv and 'egg_info' not in sys.argv:
     from .chatterbot import ChatBot
 
 __version__ = '0.4.7'
