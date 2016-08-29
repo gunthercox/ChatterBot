08300895dc8d2abb740dd71b027e9acda8bb84dd
Gunther Cox <gunthercx@gmail.com>
2016-08-29 13:09:40 -0400

    Initialize ChatterBot in django view module instead of settings.

--- chatterbot/ext/django_chatterbot/views.py
+++ chatterbot/ext/django_chatterbot/views.py
@@ -1,14 +1,23 @@
 from django.views.generic import View
 from django.http import JsonResponse
 from django.conf import settings
+from chatterbot import ChatBot
 
 
 class ChatterBotView(View):
 
+    chatterbot = ChatBot(
+        settings.CHATTERBOT['NAME'],
+        storage_adapter='chatterbot.adapters.storage.DjangoStorageAdapter',
+        input_adapter='chatterbot.adapters.input.VariableInputTypeAdapter',
+        output_adapter='chatterbot.adapters.output.OutputFormatAdapter',
+        output_format='json'
+    )
+
     def post(self, request, *args, **kwargs):
         input_statement = request.POST.get('text')
 
-        response_data = settings.CHATTERBOT.get_response(input_statement)
+        response_data = self.chatterbot.get_response(input_statement)
 
         return JsonResponse(response_data, status=200)
 
