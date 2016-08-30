35760762280d082f777545dc11ca2be0355ecfaf
davi_zucon <davi.zucon@gmail.com>
2016-08-30 10:01:59 -0300

    Avoid problems in str.format() with char accents

--- chatterbot/utils/read_input.py
+++ chatterbot/utils/read_input.py
@@ -8,6 +8,12 @@ def input_function():
     """
     if sys.version_info[0] < 3:
         user_input = str(raw_input())
+
+        # Avoid problems using format strings with unicode characters
+        if user_input:
+            user_input = user_input.decode('utf-8')
+
     else:
         user_input = input()
+        
     return user_input
