b50633f6af3ddbc6d78dc5f5837aab5b81907ae9
Gunther Cox <gunthercx@gmail.com>
2016-09-03 14:46:09 -0400

    Make it easier to extend the base test case.

--- tests/test_adapter_validation.py
+++ tests/test_adapter_validation.py
@@ -4,66 +4,70 @@ from .base_case import ChatBotTestCase
 
 class AdapterValidationTests(ChatBotTestCase):
 
+    def setUp(self):
+        super(AdapterValidationTests, self).setUp()
+        self.database_path = self.chatbot.storage.database.path
+
     def test_invalid_storage_adapter(self):
         with self.assertRaises(ChatBot.InvalidAdapterException):
             self.chatbot = ChatBot(
-                "Test Bot",
-                storage_adapter="chatterbot.adapters.input.TerminalAdapter",
+                'Test Bot',
+                storage_adapter='chatterbot.adapters.input.TerminalAdapter',
                 database=self.database_path
             )
 
     def test_valid_storage_adapter(self):
         try:
             self.chatbot = ChatBot(
-                "Test Bot",
-                storage_adapter="chatterbot.adapters.storage.JsonFileStorageAdapter",
+                'Test Bot',
+                storage_adapter='chatterbot.adapters.storage.JsonFileStorageAdapter',
                 database=self.database_path
             )
         except ChatBot.InvalidAdapterException:
-            self.fail("Test raised InvalidAdapterException unexpectedly!")
+            self.fail('Test raised InvalidAdapterException unexpectedly!')
 
     def test_invalid_input_adapter(self):
         with self.assertRaises(ChatBot.InvalidAdapterException):
             self.chatbot = ChatBot(
-                "Test Bot",
-                input_adapter="chatterbot.adapters.storage.JsonFileStorageAdapter",
+                'Test Bot',
+                input_adapter='chatterbot.adapters.storage.JsonFileStorageAdapter',
                 database=self.database_path
             )
 
     def test_valid_input_adapter(self):
         try:
             self.chatbot = ChatBot(
-                "Test Bot",
-                input_adapter="chatterbot.adapters.input.TerminalAdapter",
+                'Test Bot',
+                input_adapter='chatterbot.adapters.input.TerminalAdapter',
                 database=self.database_path
             )
         except ChatBot.InvalidAdapterException:
-            self.fail("Test raised InvalidAdapterException unexpectedly!")
+            self.fail('Test raised InvalidAdapterException unexpectedly!')
 
     def test_invalid_output_adapter(self):
         with self.assertRaises(ChatBot.InvalidAdapterException):
             self.chatbot = ChatBot(
-                "Test Bot",
-                output_adapter="chatterbot.adapters.input.TerminalAdapter",
+                'Test Bot',
+                output_adapter='chatterbot.adapters.input.TerminalAdapter',
                 database=self.database_path
             )
 
     def test_valid_output_adapter(self):
         try:
             self.chatbot = ChatBot(
-                "Test Bot",
-                output_adapter="chatterbot.adapters.output.TerminalAdapter",
+                'Test Bot',
+                output_adapter='chatterbot.adapters.output.TerminalAdapter',
                 database=self.database_path
             )
         except ChatBot.InvalidAdapterException:
-            self.fail("Test raised InvalidAdapterException unexpectedly!")
+            self.fail('Test raised InvalidAdapterException unexpectedly!')
 
     def test_invalid_logic_adapter(self):
         with self.assertRaises(ChatBot.InvalidAdapterException):
             self.chatbot = ChatBot(
-                "Test Bot",
+                'Test Bot',
                 logic_adapters=[
-                    "chatterbot.adapters.input.TerminalAdapter",
+                    'chatterbot.adapters.input.TerminalAdapter',
                 ],
                 database=self.database_path
             )
@@ -71,14 +75,14 @@ class AdapterValidationTests(ChatBotTestCase):
     def test_valid_logic_adapter(self):
         try:
             self.chatbot = ChatBot(
-                "Test Bot",
+                'Test Bot',
                 logic_adapters=[
-                    "chatterbot.adapters.logic.ClosestMatchAdapter"
+                    'chatterbot.adapters.logic.ClosestMatchAdapter'
                 ],
                 database=self.database_path
             )
         except ChatBot.InvalidAdapterException:
-            self.fail("Test raised InvalidAdapterException unexpectedly!")
+            self.fail('Test raised InvalidAdapterException unexpectedly!')
 
 
 class MultiAdapterTests(ChatBotTestCase):
@@ -87,6 +91,6 @@ class MultiAdapterTests(ChatBotTestCase):
         count_before = len(self.chatbot.logic.adapters)
 
         self.chatbot.add_adapter(
-            "chatterbot.adapters.logic.ClosestMatchAdapter"
+            'chatterbot.adapters.logic.ClosestMatchAdapter'
         )
         self.assertEqual(len(self.chatbot.logic.adapters), count_before + 1)
