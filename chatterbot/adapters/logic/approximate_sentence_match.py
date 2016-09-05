9621cc24db2967fba5e42dc30767b7375475b6c8
Gunther Cox <gunthercx@gmail.com>
2016-09-05 11:32:09 -0400

    Correct autodoc warnings.

--- chatterbot/adapters/logic/approximate_sentence_match.py
+++ chatterbot/adapters/logic/approximate_sentence_match.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
-# Imports
+from chatterbot.adapters import Adapter
 from .base_match import BaseMatchAdapter
+from nltk.corpus import wordnet
 import nltk.corpus
 import nltk.tokenize.punkt
 import nltk.stem.snowball
-from nltk.corpus import wordnet
 import string
-from chatterbot.adapters import Adapter
+
 
 class ApproximateSentenceMatchAdapter(BaseMatchAdapter):
     """
     The Jaccard index is composed of a numerator and denominator.
     In the numerator, we count the number of items that are shared between the sets.
     In the denominator, we count the total number of items across both sets.
-    Let’s say we define sentences to be equivalent if 50% or more of their tokens are equivalent.  Here are two sample sentences:
+    Let’s say we define sentences to be equivalent if 50% or more of their tokens are equivalent.
+    Here are two sample sentences:
+
         The young cat is hungry.
         The cat is very hungry.
+
     When we parse these sentences to remove stopwords, we end up with the following two sets:
+
         {young, cat, hungry}
         {cat, very, hungry}
+
     In our example above, our intersection is {cat, hungry}, which has count of two.
     The union of the sets is {young, cat, very, hungry}, which has a count of four.
     Therefore, our Jaccard similarity index is two divided by four, or 50%.
