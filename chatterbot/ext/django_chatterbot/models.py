32fd5b0875e647472b8c290e30a759f42206889a
Hobson Lane <github@totalgood.com>
2016-09-13 22:03:59 -0700

    docstrings in models

--- chatterbot/ext/django_chatterbot/models.py
+++ chatterbot/ext/django_chatterbot/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Statement(models.Model):
+    """A short (<255) chat message, tweet, forum post, etc"""
 
     text = models.CharField(
         unique=True,
@@ -11,10 +12,23 @@ class Statement(models.Model):
     )
 
     def __str__(self):
-        return '{}...'.format(self.text[:57]) if len(self.text) > 60 else self.text
+        if len(self.text.strip()) > 60:
+            return '{}...'.format(self.text[:57])
+        elif len(self.text.strip()) > 0:
+            return self.text
+        return '<empty>'
 
 
 class Response(models.Model):
+    """Connection between a response and the statement that triggered it
+
+    Comparble to a ManyToMany "through" table, but without the M2M indexing/relations.
+
+    Only the text and number of times it has occurred are currently stored.
+    Might be useful to store additional features like language, location(s)/region(s),
+    first created datetime(s), username, user full name, user gender, etc.
+    A the very least occurrences should be an FK to a meta-data table with this info.
+    """
 
     statement = models.ForeignKey(
         'Statement',
@@ -32,6 +46,6 @@ class Response(models.Model):
 
     def __str__(self):
         s = self.statement.text if len(self.statement.text) <= 20 else self.statement.text[:17] + '...'
-        s += '\n => '
+        s += ' => '
         s += self.response.text if len(self.response.text) <= 40 else self.statement.text[:37] + '...'
         return s
