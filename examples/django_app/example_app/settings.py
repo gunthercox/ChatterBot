a9ea1a27a08fc2465c3b4fabc1a66aad44c1ad75
Gunther Cox <gunthercx@gmail.com>
2016-09-19 12:43:07 -0400

    Add tests for getting recent response api data.

--- examples/django_app/example_app/settings.py
+++ examples/django_app/example_app/settings.py
@@ -39,7 +39,6 @@ CHATTERBOT = {
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
---
+++ examples/django_app/tests/test_integration.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+import unittest
+
+
+class ApiIntegrationTestCase(TestCase):
+
+    def setUp(self):
+        super(ApiIntegrationTestCase, self).setUp()
+        self.api_url = reverse('chatterbot:chatterbot')
+
+    def _get_json(self, response):
+        import json
+        return json.loads(response.content)
+
+    def test_get_recent_statements_empty(self):
+        response = self.client.get(self.api_url)
+        data = self._get_json(response)
+
+        unittest.SkipTest('This test needs to be created.')
+
+    def test_get_recent_statements(self):
+        response = self.client.post(
+            self.api_url,
+            {'text': 'How are you?'},
+            format='json'
+        )
+
+        response = self.client.get(self.api_url)
+        data = self._get_json(response)
+
+        self.assertIn('recent_statements', data)
+        self.assertEqual(len(data['recent_statements']), 1)
+        self.assertEqual(len(data['recent_statements'][0]), 2)
+        self.assertIn('text', data['recent_statements'][0][0])
+        self.assertIn('text', data['recent_statements'][0][1])
