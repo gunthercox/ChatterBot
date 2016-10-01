4c25c63e7d6e1b9eeebd7d30637984e586eaf9eb
Gunther Cox <gunthercx@gmail.com>
2016-10-01 09:06:47 -0400

    Add methods for adding and removing logic adapters.

--- tests/test_adapter_validation.py
+++ tests/test_adapter_validation.py
@@ -94,3 +94,35 @@ class MultiAdapterTests(ChatBotTestCase):
             'chatterbot.adapters.logic.ClosestMatchAdapter'
         )
         self.assertEqual(len(self.chatbot.logic.adapters), count_before + 1)
+
+    def test_insert_logic_adapter(self):
+        self.chatbot.add_adapter('chatterbot.adapters.logic.TimeLogicAdapter')
+        self.chatbot.add_adapter('chatterbot.adapters.logic.ClosestMatchAdapter')
+
+        self.chatbot.insert_logic_adapter('chatterbot.adapters.logic.MathematicalEvaluation', 1)
+
+        self.assertEqual(
+            type(self.chatbot.logic.adapters[1]).__name__,
+            'MathematicalEvaluation'
+        )
+
+    def test_remove_logic_adapter(self):
+        self.chatbot.add_adapter('chatterbot.adapters.logic.TimeLogicAdapter')
+        self.chatbot.add_adapter('chatterbot.adapters.logic.MathematicalEvaluation')
+
+        adapter_count = len(self.chatbot.logic.adapters)
+
+        removed = self.chatbot.remove_logic_adapter('MathematicalEvaluation')
+
+        self.assertTrue(removed)
+        self.assertEqual(len(self.chatbot.logic.adapters), adapter_count - 1)
+
+    def test_remove_logic_adapter_not_found(self):
+        self.chatbot.add_adapter('chatterbot.adapters.logic.TimeLogicAdapter')
+
+        adapter_count = len(self.chatbot.logic.adapters)
+
+        removed = self.chatbot.remove_logic_adapter('MathematicalEvaluation')
+
+        self.assertFalse(removed)
+        self.assertEqual(len(self.chatbot.logic.adapters), adapter_count)
