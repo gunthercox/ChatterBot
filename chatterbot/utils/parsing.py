42330ce70ccaf617ebfdcb1c19d1e6ca542fcd96
Vinay <rmdort@gmail.com>
2016-10-04 12:11:59 +0800

    Added more tests + patterns

--- chatterbot/utils/parsing.py
+++ chatterbot/utils/parsing.py
@@ -4,88 +4,340 @@ import calendar
 
 day_names = 'monday|tuesday|wednesday|thursday|friday|saturday|sunday'
 month_names = 'january|february|march|april|may|june|july|august|september|october|november|december|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec'
-day_nearest_names = 'today|yesterday|tomorrow|tonight'
+day_nearest_names = 'today|yesterday|tomorrow|tonight|tonite'
 numbers = "(^a(?=\s)|one|two|three|four|five|six|seven|eight|nine|ten| \
                     eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen| \
                     eighteen|nineteen|twenty|thirty|forty|fifty|sixty|seventy|eighty| \
                     ninety|hundred|thousand)"
 re_dmy = '(year|day|week|month|night|minute|min)'
-re_duration = '(before|after|earlier|later|ago|from now)'
+re_duration = '(before|after|earlier|later|ago|from\snow)'
 re_year = "((?<=\s)\d{4}|^\d{4})"
+re_timeframe = 'this|next|following|previous|last'
 
+# A list tuple of regular expressions / parser fn to match
+# The order of the match in this list matters, So always start with the widest match and narrow it down
 regex = [
     (re.compile(
         r'''
         (
-            (%s) # day_names
-            [,\s]\s*\d{1,2} # Any digit
+            (?P<dow>%s) # day_names
+            [,\s]\s*(?P<day>\d{1,2}) # Any digit
             \s+ # One or more space
-            (%s) # Month names
+            (?P<month>%s) # Month names
             \s+ # One or more space
-            %s # Year
+            (?P<year>%s) # Year
         )
         '''% (day_names, month_names, re_year),
         (re.VERBOSE | re.IGNORECASE)
         ),
-        lambda m: datetime.today()
+        lambda (m, base_date): datetime(
+                int(m.group('year')),
+                hashmonths[m.group('month').lower()],
+                int(m.group('day'))
+            )
     ),
     (re.compile(
         r'''
         (
-            \d{1,2}
-            \s+
-            %s
-            \s
-            %s # Year
+            (?P<day>\d{1,2}) # Matches a digit
+            \s+ # One or more space
+            (?P<month>%s) # Matches any month name
+            \s # Space
+            (?P<year>%s) # Year
         )
         '''% (month_names, re_year),
         (re.VERBOSE | re.IGNORECASE)
         ),
-    lambda m: datetime.today()
+        lambda (m, base_date): datetime(
+                base_date.year,
+                hashmonths[m.group('month').lower()],
+                int(m.group('day'))
+            )
     ),
     (re.compile(
         r'''
         (
-            (?P<number>\d+|(%s[-\s]?)+)\s
-            (?P<unit>%ss?\s)
-            (?P<duration>%s)
+            ((?P<number>\d+|(%s[-\s]?)+)\s)? # Matches any number or string 25 or twenty five
+            (?P<unit>%s)s?\s # Matches days, months, years, weeks, minutes
+            (?P<duration>%s) # before, after, earlier, later, ago, from now
+            (\s*(?P<base_time>(%s)))?
         )
-        '''% (numbers, re_dmy, re_duration),
+        '''% (numbers, re_dmy, re_duration, day_nearest_names),
         (re.VERBOSE | re.IGNORECASE)
         ),
-        lambda m: m.group('number') + m.group('unit') + m.group('duration')
+        lambda (m, base_date): dateFromDuration(
+            base_date,
+            m.group('number'),
+            m.group('unit').lower(),
+            m.group('duration').lower(),
+            m.group('base_time')
+        )
     ),
     (re.compile(
         r'''
-        (%s)
+        (?P<time>%s) # this, next, following, previous, last
+        \s+
+        (?P<dow>%s) # mon - fri
+        '''% (re_timeframe, day_names),
+        (re.VERBOSE | re.IGNORECASE),
+        ),
+        lambda (m, base_date): dateFromRelativeDay(base_date, m.group('time'), m.group('dow'))
+    ),
+    (re.compile(
+        r'''
+        (?P<time>%s) # this, next, following, previous, last
+        \s+
+        (?P<dow>%s) # year, day, week, month, night, minute, min
+        '''% (re_timeframe, re_dmy),
+        (re.VERBOSE | re.IGNORECASE),
+        ),
+        lambda (m, base_date): dateFromRelativeWeekYear(base_date, m.group('time'), m.group('dow'))
+    ),
+    (re.compile(
+        r'''
+        (?P<adverb>%s) # today, yesterday, tomorrow, tonight
         '''% (day_nearest_names),
         (re.VERBOSE | re.IGNORECASE)
         ),
-        lambda m: datetime.today()
+        lambda (m, base_date): dateFromAdverb(base_date, m.group('adverb'))
     ),
     (re.compile(
         r'''
-        (%s)
-        '''% day_names,
-        (re.VERBOSE | re.IGNORECASE),
+        (?P<year>%s) # Year
+        '''% (re_year),
+        (re.VERBOSE | re.IGNORECASE)
         ),
-        lambda m: datetime.today()
-    )
+        lambda (m, base_date): datetime(int(m.group('year')), 1, 1)
+    ),
 ]
 
+# Hash of numbers
+# Append more number to modify your match
+def hashnum(number):
+    if re.match(r'one|^a\b', number, re.IGNORECASE):
+        return 1
+    if re.match(r'two', number, re.IGNORECASE):
+        return 2
+    if re.match(r'three', number, re.IGNORECASE):
+        return 3
+    if re.match(r'four', number, re.IGNORECASE):
+        return 4
+    if re.match(r'five', number, re.IGNORECASE):
+        return 5
+    if re.match(r'six', number, re.IGNORECASE):
+        return 6
+    if re.match(r'seven', number, re.IGNORECASE):
+        return 7
+    if re.match(r'eight', number, re.IGNORECASE):
+        return 8
+    if re.match(r'nine', number, re.IGNORECASE):
+        return 9
+    if re.match(r'ten', number, re.IGNORECASE):
+        return 10
+    if re.match(r'eleven', number, re.IGNORECASE):
+        return 11
+    if re.match(r'twelve', number, re.IGNORECASE):
+        return 12
+    if re.match(r'thirteen', number, re.IGNORECASE):
+        return 13
+    if re.match(r'fourteen', number, re.IGNORECASE):
+        return 14
+    if re.match(r'fifteen', number, re.IGNORECASE):
+        return 15
+    if re.match(r'sixteen', number, re.IGNORECASE):
+        return 16
+    if re.match(r'seventeen', number, re.IGNORECASE):
+        return 17
+    if re.match(r'eighteen', number, re.IGNORECASE):
+        return 18
+    if re.match(r'nineteen', number, re.IGNORECASE):
+        return 19
+    if re.match(r'twenty', number, re.IGNORECASE):
+        return 20
+    if re.match(r'thirty', number, re.IGNORECASE):
+        return 30
+    if re.match(r'forty', number, re.IGNORECASE):
+        return 40
+    if re.match(r'fifty', number, re.IGNORECASE):
+        return 50
+    if re.match(r'sixty', number, re.IGNORECASE):
+        return 60
+    if re.match(r'seventy', number, re.IGNORECASE):
+        return 70
+    if re.match(r'eighty', number, re.IGNORECASE):
+        return 80
+    if re.match(r'ninety', number, re.IGNORECASE):
+        return 90
+    if re.match(r'hundred', number, re.IGNORECASE):
+        return 100
+    if re.match(r'thousand', number, re.IGNORECASE):
+      return 1000
+
+# Convert strings to numbers
+def convert_string_to_number(value):
+    if value == None:
+        return 1
+    if isinstance(value, int):
+        return value
+    if value.isdigit():
+        return int(value)
+    num_list = map(lambda s:hashnum(s), re.findall(numbers + '+', value, re.IGNORECASE))
+    return sum(num_list)
+
+
+# Converts relative day to time
+# this tuesday, last tuesday
+def dateFromRelativeDay(base_date, time, dow):
+    time = time.lower()
+    dow = dow.lower()
+    if time == 'this':
+        # Else day of week
+        num = hashweekdays[dow]
+        return this_week_day(base_date, num)
+    elif time == 'last' or time == 'previous':
+        # Else day of week
+        num = hashweekdays[dow]
+        return previous_week_day(base_date, num)
+    elif time == 'next' or time == 'following':
+        # Else day of week
+        num = hashweekdays[dow]
+        return next_week_day(base_date, num)
+
+# Converts relative day to time
+# this tuesday, last tuesday
+def dateFromRelativeWeekYear(base_date, time, dow):
+    if dow == 'year' or dow == 'years':
+        if time == 'this':
+            return datetime(base_date.year, 1, 1)
+        elif time == 'last' or time == 'previous':
+            return datetime(base_date.year - 1, base_date.month, base_date.day)
+    elif dow == 'week' or dow == 'weeks':
+        if time == 'this':
+            return base_date - timedelta(days=base_date.weekday())
+        elif time == 'last' or time == 'previous':
+            return base_date - timedelta(weeks=1)
+
+# Convert Day adverbs to dates
+# Tomorrow => Date
+# Today => Date
+def dateFromAdverb(base_date, name):
+    if name == 'today' or name == 'tonite' or name == 'tonight':
+        return datetime.today()
+    elif name == 'yesterday':
+        return base_date - timedelta(days=1)
+    elif name == 'tomorrow' or name == 'tom':
+        return base_date + timedelta(days=1)
+
+# Find dates from duration
+# Eg: 20 days from now
+# Doesnt support 20 days from last monday
+def dateFromDuration(base_date, numberAsString, unit, duration, base_time = None):
+    # Check if query is `2 days before yesterday` or `day before yesterday`
+    if base_time != None:
+        base_date = dateFromAdverb(base_date, base_time)
+    num = convert_string_to_number(numberAsString)
+    if unit == 'day' or unit == 'days':
+        args = {'days': num}
+    elif unit == 'minute' or unit == 'mins' or unit == 'minutes':
+        args = {'minutes': num}
+    elif unit == 'week' or unit == 'weeks' or unit == 'wks':
+        args = {'weeks': num}
+    elif unit == 'year' or unit == 'years' or unit == 'yrs':
+        args = {'years': num}
+
+    if duration == 'ago' or duration == 'before' or duration == 'earlier':
+        if ('years' in args):
+            return datetime(base_date.year - args['years'], base_date.month, base_date.day)
+        return base_date - timedelta(**args)
+    elif duration == 'after' or duration == 'later' or duration == 'from now':
+        if ('years' in args):
+            return datetime(base_date.year + args['years'], base_date.month, base_date.day)
+        return base_date + timedelta(**args)
+
+# Finds coming weekday
+def this_week_day(base_date, weekday):
+    day_of_week = base_date.weekday()
+    # If today is Tuesday and the query is `this monday`
+    # We should output the next_week monday
+    if day_of_week > weekday:
+        return next_week_day(base_date, weekday)
+    start_of_this_week = base_date - timedelta(days=day_of_week + 1)
+    day = start_of_this_week + timedelta(days=1)
+    while day.weekday() != weekday:
+        day = day + timedelta(days=1)
+    return day
+
+# Finds coming weekday
+def previous_week_day(base_date, weekday):
+    day = base_date.today() - timedelta(days=1)
+    while day.weekday() != weekday:
+        day = day - timedelta(days=1)
+    return day
+
+def next_week_day(base_date, weekday):
+    day_of_week = base_date.weekday()
+    end_of_this_week = base_date + timedelta(days=6 - day_of_week)
+    day = end_of_this_week + timedelta(days=1)
+    while day.weekday() != weekday:
+        day = day + timedelta(days=1)
+    return day
+
+
+# Mapping of Month name and Value
+hashmonths = {
+    'january': 1,
+    'jan': 1,
+    'february': 2,
+    'feb': 2,
+    'march': 3,
+    'mar': 3,
+    'april': 4,
+    'apr': 4,
+    'may': 5,
+    'june': 6,
+    'jun': 6,
+    'july': 7,
+    'jul': 7,
+    'august': 8,
+    'aug': 8,
+    'september': 9,
+    'sep': 9,
+    'october': 10,
+    'oct': 10,
+    'november': 11,
+    'nov': 11,
+    'december': 12,
+    'dec': 12
+}
+
+# Days to number mapping
+hashweekdays = {
+    'monday': 0,
+    'mon': 0,
+    'tuesday': 1,
+    'tue': 1,
+    'wednesday': 2,
+    'wed': 2,
+    'thursday': 3,
+    'thu': 3,
+    'friday': 4,
+    'fri': 4,
+    'saturday': 5,
+    'sat': 5,
+    'sunday': 6,
+    'sun': 6}
+
 # Parses date
-def datetime_parsing (text):
+def datetime_parsing (text, base_date = datetime.now()):
     matches = []
     found_array = []
-    resolved_array = []
-    original_text = text
 
     # Find the position in the string
     for r, fn in regex:
         for m in r.finditer(text):
-            matches.append((m.group(), fn(m), m.span()))
+            matches.append((m.group(), fn((m, base_date)), m.span()))
 
-    # We need to wrap the matched text with TAG element to prevent nested selections
+    # Wrap the matched text with TAG element to prevent nested selections
     for match, value, spans in matches:
         subn = re.subn('(?!<TAG[^>]*?>)' + match + '(?![^<]*?</TAG>)', '<TAG>' + match + '</TAG>', text)
         text = subn[0]
@@ -93,4 +345,8 @@ def datetime_parsing (text):
         if isSubstituted != 0:
             found_array.append((match, value, spans))
 
-    return found_array
+    # To preserve order of the match, sort based on the start position
+    return sorted(found_array, key = lambda match: match and match[2][0])
+
+
+print datetime_parsing('20 days from last monday')
---
+++ tests/test_parsing.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+from datetime import timedelta, date, datetime
+from chatterbot.utils.parsing import datetime_parsing, this_week_day, previous_week_day, next_week_day, dateFromDuration
+
+"""
+  Output of the parser is a tuple
+  [match, value, (start, end)]
+"""
+
+class DateTimeParsingTestCases(TestCase):
+  def test_captured_patterns(self):
+    base_date = datetime.now()
+
+    input_text = 'The event is on Monday 12 January 2012'
+    parser = datetime_parsing(input_text)
+    self.assertIn('Monday 12 January 2012', parser[0])
+    self.assertEqual(parser[0][1], datetime(2012, 1, 12))
+    self.assertEqual(len(parser), 1)
+
+    input_text = 'This monday'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%d-%m-%y'), this_week_day(base_date, 0).strftime('%d-%m-%y'))
+    self.assertEqual(len(parser), 1)
+
+    input_text = 'Last monday'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%d-%m-%y'), previous_week_day(base_date, 0).strftime('%d-%m-%y'))
+    self.assertEqual(len(parser), 1)
+
+    input_text = 'Next monday'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%d-%m-%y'), next_week_day(base_date, 0).strftime('%d-%m-%y'))
+    self.assertEqual(len(parser), 1)
+
+    input_text = '25 minutes from now'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%d-%m-%y'), dateFromDuration(base_date, 25, 'minutes', 'from now').strftime('%d-%m-%y'))
+    self.assertEqual(len(parser), 1)
+
+    input_text = '10 days later'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%d-%m-%y'), dateFromDuration(base_date, 10, 'days', 'later').strftime('%d-%m-%y'))
+    self.assertEqual(len(parser), 1)
+
+    input_text = '2010'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%Y'), input_text)
+    self.assertEqual(len(parser), 1)
+
+    input_text = 'today'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%d'), datetime.today().strftime('%d'))
+    self.assertEqual(len(parser), 1)
+
+    input_text = 'tomorrow'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%d'), (datetime.today() + timedelta(days=1)).strftime('%d'))
+    self.assertEqual(len(parser), 1)
+
+    input_text = 'yesterday'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%d'), (datetime.today() - timedelta(days=1)).strftime('%d'))
+    self.assertEqual(len(parser), 1)
+
+    input_text = 'day before yesterday'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%d'), (datetime.today() - timedelta(days=2)).strftime('%d'))
+    self.assertEqual(len(parser), 1)
+
+    input_text = 'day before today'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%d'), (datetime.today() - timedelta(days=1)).strftime('%d'))
+    self.assertEqual(len(parser), 1)
+
+    input_text = 'day before tomorrow'
+    parser = datetime_parsing(input_text)
+    self.assertIn(input_text, parser[0])
+    self.assertEqual(parser[0][1].strftime('%d'), (datetime.today() - timedelta(days=0)).strftime('%d'))
+    self.assertEqual(len(parser), 1)
