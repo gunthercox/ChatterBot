e057d01eb227d81280a079969174bc8c61a4a7ff
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2016-10-30 14:17:19 +0530

    nltk download loop

--- chatterbot/utils/wordnet.py
+++ chatterbot/utils/wordnet.py
@@ -11,9 +11,19 @@ class Wordnet(object):
     def __init__(self):
         from nltk.data import find
         from nltk import download
-
+        import os
+        
+        # Download the wordnet data only if it is not already downloaded
+        wordnet_path = None
+        if os.name == 'nt':
+            wordnet_path = os.path.join(os.getenv('APPDATA'), 'nltk_data',
+                                                'corpora', 'wordnet.zip')
+        else:
+            wordnet_path = os.path.join(os.path.expanduser('~'), 'nltk_data',
+                                                'corpora', 'wordnet.zip')
         try:
-            find('wordnet.zip')
+            if not os.path.isfile(wordnet_path):
+                find('wordnet.zip')
         except LookupError:
             download('wordnet')
 
