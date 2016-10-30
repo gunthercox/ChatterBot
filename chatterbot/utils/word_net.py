0e6aaf35fed10c80fbb76c0ebae7cead9c57f80d
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2016-10-30 09:54:57 +0530

    Issue 350
    https://github.com/gunthercox/ChatterBot/issues/350

--- chatterbot/utils/word_net.py
+++ chatterbot/utils/word_net.py
@@ -11,8 +11,15 @@ class Wordnet():
     def __init__(self):
         from nltk.data import find
         from nltk import download
+        import os
 
         try:
+            if os.name == 'nt':
+                nltk.data.path.append(os.path.join(os.getenv('APPDATA',
+                                                            'nltk_data')))
+            else:
+                nltk.data.path.append(os.path.join(os.path.expanduser('~'), 
+                                                            'nltk_data'))
             find('wordnet.zip')
         except LookupError:
             download('wordnet')
