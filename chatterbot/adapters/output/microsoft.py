f69d22173dca774d29e828cd7d91aeaeb2728513
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2016-11-04 22:47:06 +0530

    microsoft output adapter

---
+++ chatterbot/adapters/output/microsoft.py
@@ -0,0 +1,75 @@
+from chatterbot.adapters.output import OutputAdapter
+import requests
+import json
+
+
+class Microsoft(OutputAdapter):
+    """
+    An output adapter that allows a ChatterBot instance to send
+    responses to a HipChat room.
+    """
+
+    def __init__(self, **kwargs):
+        super(Microsoft, self).__init__(**kwargs)
+
+        self.directline_host = kwargs.get("directline_host")
+        self.direct_line_token = kwargs.get("direct_line_token")
+        self.conversation_id = kwargs.get("conversation_id")
+
+        authorization_header = 'BotConnector  {}'.format(self.direct_line_token)
+
+        self.headers = {
+            'Authorization': authorization_header,
+            'Content-Type': 'application/json'
+        }
+
+    def get_most_recent_message(self, watermark='1'):
+        endpoint = '{host}/api/conversations/{id}/messages?watermark={watermark}'\
+            .format(host=self.directline_host,
+                    id=self.conversation_id,
+                    watermark=watermark)
+
+        response = requests.get(
+            endpoint,
+            headers=self.headers
+        )
+        self.logger.info(u'{} getting most recent message'.format(
+            response.status_code
+        ))
+        self._validate_status_code(response)
+        data = response.json()
+        if data["messages"]:
+            return data["messages"][0]
+        return None
+
+    def send_message(self, conversation_id, message):
+        """
+        Send a message to a HipChat room.
+        https://www.hipchat.com/docs/apiv2/method/send_message
+        """
+
+        message_url = "{host}/api/conversations/{conversationId}/messages".\
+            format(host=self.directline_host, conversationId=conversation_id)
+
+        response = requests.post(
+            message_url,
+            headers=self.headers,
+            data=json.dumps({
+                'message': message
+            })
+        )
+
+        # Microsoft return 204 operation succeeded and no content was returned.
+        return self.get_most_recent_message()
+
+    def process_response(self, statement, confidence=None):
+        data = self.send_message(self.conversation_id, statement.text)
+
+
+
+        # Update the output statement with the message id
+        self.context.recent_statements[-1][1].add_extra_data(
+            'microsoft_msg_id', data['id']
+        )
+
+        return statement
