f12b91eb4a689bbbd4479ae62e4569cdab12bcba
Gunther Cox <gunthercx@gmail.com>
2016-11-05 09:19:43 -0400

    Don't overide get method on ApproximateSentenceMatchAdapter

--- chatterbot/adapters/logic/approximate_sentence_match.py
+++ chatterbot/adapters/logic/approximate_sentence_match.py
@@ -12,28 +12,3 @@ class ApproximateSentenceMatchAdapter(BaseMatchAdapter):
             'statement_comparison_function',
             jaccard_similarity
         )
-
-    def get(self, input_statement):
-        """
-        Takes a statement string and a list of statement strings.
-        Returns the closest matching statement from the list.
-        """
-        statement_list = self.context.storage.get_response_statements()
-
-        if not statement_list:
-            if self.has_storage_context:
-                # Use a randomly picked statement
-                return 0, self.context.storage.get_random()
-            else:
-                raise self.EmptyDatasetException()
-
-        confidence = -1
-        sentence_match = input_statement
-        # Find the matching known statement
-        for statement in statement_list:
-            ratio = self.compare_statements(input_statement, statement)
-            if ratio:
-                closest_match = statement
-            else:
-                closest_match = statement
-        return 0.5, closest_match
