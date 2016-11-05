ff0fb4863fada0daa21b0bb95788b3fe386a84f8
Gunther Cox <gunthercx@gmail.com>
2016-11-05 07:45:22 -0400

    Move matching logic adapter methods to parent class
    The closest match adapter and closest meaning adapter
    share very similar code that can now be combined into
    the parent class that they both share.
    
    In the future, the two subclasses will be removed in
    favor of just setting the comparison_function parameter.

--- chatterbot/adapters/logic/closest_match.py
+++ chatterbot/adapters/logic/closest_match.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from fuzzywuzzy import fuzz
 from .base_match import BaseMatchAdapter
 
 
@@ -11,33 +10,11 @@ class ClosestMatchAdapter(BaseMatchAdapter):
     of each statement.
     """
 
-    def get(self, input_statement):
-        """
-        Takes a statement string and a list of statement strings.
-        Returns the closest matching statement from the list.
-        """
-        statement_list = self.context.storage.get_response_statements()
+    def __init__(self, **kwargs):
+        super(ClosestMatchAdapter, self).__init__(**kwargs)
+        from chatterbot.conversation.comparisons import levenshtein_distance
 
-        if not statement_list:
-            if self.has_storage_context:
-                # Use a randomly picked statement
-                self.logger.info(
-                    u'No statements have known responses. ' +
-                    u'Choosing a random response to return.'
-                )
-                return 0, self.context.storage.get_random()
-            else:
-                raise self.EmptyDatasetException()
-
-        closest_match = input_statement
-        max_confidence = 0
-
-        # Find the closest matching known statement
-        for statement in statement_list:
-            confidence = self.compare_statements(input_statement, statement)
-
-            if confidence > max_confidence:
-                max_confidence = confidence
-                closest_match = statement
-
-        return max_confidence, closest_match
+        self.compare_statements = kwargs.get(
+            'statement_comparison_function',
+            levenshtein_distance
+        )
