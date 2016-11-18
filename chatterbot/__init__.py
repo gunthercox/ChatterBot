307e514e0ca0fe89879318fb28ea38f4936ac3d8
Gunther Cox <gunthercx@gmail.com>
2016-11-17 19:11:21 -0500

    Updated version string to 0.4.13

--- chatterbot/__init__.py
+++ chatterbot/__init__.py
@@ -3,7 +3,7 @@ import sys
 if 'install' not in sys.argv and 'egg_info' not in sys.argv:
     from .chatterbot import ChatBot
 
-__version__ = '0.4.12'
+__version__ = '0.4.13'
 __author__ = 'Gunther Cox'
 __email__ = 'gunthercx@gmail.com'
 
