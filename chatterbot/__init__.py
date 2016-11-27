9df23d6d6c5f56537fa62a2bc0da632f5fca219a
Gunther Cox <gunthercx@gmail.com>
2016-11-26 19:37:33 -0500

    Update version to 0.5

--- chatterbot/__init__.py
+++ chatterbot/__init__.py
@@ -6,7 +6,7 @@ import sys
 if 'install' not in sys.argv and 'egg_info' not in sys.argv:
     from .chatterbot import ChatBot
 
-__version__ = '0.4.14'
+__version__ = '0.5.0'
 __author__ = 'Gunther Cox'
 __email__ = 'gunthercx@gmail.com'
 __url__ = 'https://github.com/gunthercox/ChatterBot'
