f6689b30279280e56a0e3f85e2b34099b81cd7cb
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2016-12-12 19:12:23 +0530

    response into JSON object

--- chatterbot/ext/django_chatterbot/views.py
+++ chatterbot/ext/django_chatterbot/views.py
@@ -66,6 +66,15 @@ class ChatterBotView(ChatterBotViewMixin, View):
 
         response_data = self.chatterbot.get_response(input_data, chat_session.id_string)
 
+       if isinstance(response_data, basestring):
+            response_data = response_data.encode('utf8')
+        else:
+            response_data = unicode(response_data).encode('utf8')
+
+        response_data = {
+            'text': response_data
+        }
+
         return JsonResponse(response_data, status=200)
 
     def get(self, request, *args, **kwargs):
