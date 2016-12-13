22a0a583f8b8f986ea031953159eb990e68bd399
Gunther Cox <gunthercx@gmail.com>
2016-12-13 10:09:59 -0500

    Remove deprecated sentiment adapter class

--- chatterbot/logic/__init__.py
+++ chatterbot/logic/__init__.py
@@ -7,7 +7,6 @@ from .low_confidence import LowConfidenceAdapter
 from .mathematical_evaluation import MathematicalEvaluation
 from .multi_adapter import MultiLogicAdapter
 from .no_knowledge_adapter import NoKnowledgeAdapter
-from .sentiment_adapter import SentimentAdapter
 from .specific_response import SpecificResponseAdapter
 from .time_adapter import TimeLogicAdapter
 
