aa4b7ce5e5ce8c9d9adf365ee01e169648c104db
Gunther Cox <gunthercx@gmail.com>
2016-12-13 00:49:13 -0500

    Give the OutputAdapter class a default action

--- chatterbot/output/output_adapter.py
+++ chatterbot/output/output_adapter.py
@@ -3,13 +3,21 @@ from chatterbot.adapters import Adapter
 
 class OutputAdapter(Adapter):
     """
-    This is an abstract class that represents the
-    interface that all output adapters should implement.
+    A generic class that can be overridden by a subclass to provide extended
+    functionality, such as delivering a response to an API endpoint.
     """
 
-    def process_response(self, input_value, confidence, session_id=None):
+    def process_response(self, statement, confidence, session_id=None):
         """
-        Takes an input value.
-        Returns an output value.
+        Override this method in a subclass to implement customized functionality.
+
+        :param statement: The statement that the chat bot has produced in response to some input.
+
+        :param confidence: The confidence with which the chat bot 'thinks' the
+                           given statement is the correct response.
+
+        :param session_id: The unique id of the current chat session.
+
+        :returns: The response statement.
         """
-        raise self.AdapterMethodNotImplementedError()
+        return statement
