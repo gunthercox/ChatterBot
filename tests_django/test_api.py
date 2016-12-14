c04af67eebcbf7e0aa4f2818956737fcadb8a7db
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2016-12-14 19:44:40 +0530

    added tests for response obj

--- tests_django/test_api.py
+++ tests_django/test_api.py
@@ -27,6 +27,48 @@ class ApiTestCase(TestCase):
         self.assertIn('text', str(response.content))
         self.assertIn('in_response_to', str(response.content))
 
+    def test_response_is_in_json(self):
+        """
+        Test Response is in JSON
+        """
+        def is_json(myjson):
+            try:
+                json_object = json.loads(myjson)
+            except ValueError, e:
+                return False
+            return True
+
+        data = {
+            'text': 'Is this JSON Data?'
+        }
+        response = self.client.post(
+            self.api_url,
+            data=json.dumps(data),
+            content_type='application/json',
+            format='json'
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(is_json(response.content))
+
+    def test_unicode_post(self):
+        """
+        Test that unicode reponse
+        """
+        data = {
+            'text' : u'\u2013'
+        }
+        response = self.client.post(
+            self.api_url,
+            data=json.dumps(data),
+            content_type='application/json',
+            format=json
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('text', str(response.content))
+        self.assertIn('in_response_to', str(response.content))
+
     def test_post_extra_data(self):
         post_data = {
             'text': 'Good morning.',
