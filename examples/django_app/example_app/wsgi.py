710d0cb33efe75d6f05cd81b886f6619150a37c1
Mallikarjunarao Kosuri <venkatamallikarjunarao.kosuri@adtran.com>
2016-12-14 19:04:10 +0530

    wsgi should point eample_app

--- examples/django_app/example_app/wsgi.py
+++ examples/django_app/example_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_chatterbot.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_app.settings")
 
 application = get_wsgi_application()
