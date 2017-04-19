70caa0becd2b5a890e4ec2bfaaf6a84125abd010
Davi S. Zucon <davi.zucon@gmail.com>
2017-04-19 09:19:10 -0300

    Fiz import

--- chatterbot/storage/__init__.py
+++ chatterbot/storage/__init__.py
@@ -2,3 +2,9 @@ from .storage_adapter import StorageAdapter
 from .django_storage import DjangoStorageAdapter
 from .jsonfile import JsonFileStorageAdapter
 from .mongodb import MongoDatabaseAdapter
+
+# FIXME Better way manage import
+try:
+    from .sqlalchemy_storage import SQLAlchemyDatabaseAdapter
+except ImportError:
+    pass
