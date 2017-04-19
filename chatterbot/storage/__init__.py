44ed12d7bee40824c6216ed8247699adf766a4f6
Davi S. Zucon <davi.zucon@gmail.com>
2017-04-19 09:09:00 -0300

    Travis first install ChatterBot after install requirements...

--- chatterbot/storage/__init__.py
+++ chatterbot/storage/__init__.py
@@ -2,4 +2,3 @@ from .storage_adapter import StorageAdapter
 from .django_storage import DjangoStorageAdapter
 from .jsonfile import JsonFileStorageAdapter
 from .mongodb import MongoDatabaseAdapter
-from .sqlalchemy_storage import SQLAlchemyDatabaseAdapter
