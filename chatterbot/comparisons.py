fe1020239f00c4b44998edbc5c05b9aae7d19f72
Gunther Cox <gunthercx@gmail.com>
2017-06-02 22:42:36 -0400

    Update docstrings

--- chatterbot/comparisons.py
+++ chatterbot/comparisons.py
@@ -33,15 +33,18 @@ class Comparator:
 
 
 class LevenshteinDistance(Comparator):
+    """
+    Compare two statements based on the Levenshtein distance
+    of each statement's text.
+
+    For example, there is a 65% similarity between the statements
+    "where is the post office?" and "looking for the post office"
+    based on the Levenshtein distance algorithm.
+    """
 
     def compare(self, statement, other_statement):
         """
-        Compare two statements based on the Levenshtein distance
-        of each statement's text.
-
-        For example, there is a 65% similarity between the statements
-        "where is the post office?" and "looking for the post office"
-        based on the Levenshtein distance algorithm.
+        Compare the two input statements.
 
         :return: The percent of similarity between the text of the statements.
         :rtype: float
@@ -81,6 +84,14 @@ class LevenshteinDistance(Comparator):
 
 
 class SynsetDistance(Comparator):
+    """
+    Calculate the similarity of two statements.
+    This is based on the total maximum synset similarity between each word in each sentence.
+
+    This algorithm uses the `wordnet`_ functionality of `NLTK`_ to determine the similarity
+    of two statements based on the path similarity between each token of each statement.
+    This is essentially an evaluation of the closeness of synonyms.
+    """
 
     def initialize_nltk_wordnet(self):
         """
@@ -100,12 +111,7 @@ class SynsetDistance(Comparator):
 
     def compare(self, statement, other_statement):
         """
-        Calculate the similarity of two statements.
-        This is based on the total maximum synset similarity between each word in each sentence.
-
-        This algorithm uses the `wordnet`_ functionality of `NLTK`_ to determine the similarity
-        of two statements based on the path similarity between each token of each statement.
-        This is essentially an evaluation of the closeness of synonyms.
+        Compare the two input statements.
 
         :return: The percent of similarity between the closest synset distance.
         :rtype: float
@@ -158,10 +164,15 @@ class SynsetDistance(Comparator):
 
 
 class SentimentComparison(Comparator):
+    """
+    Calculate the similarity of two statements based on the closeness of
+    the sentiment value calculated for each statement.
+    """
 
     def initialize_nltk_vader_lexicon(self):
         """
-        Download required NLTK corpora if they have not already been downloaded.
+        Download the NLTK vader lexicon for sentiment analysis
+        that is required for this algorithm to run.
         """
         from .utils import nltk_download_corpus
 
@@ -169,8 +180,8 @@ class SentimentComparison(Comparator):
 
     def compare(self, statement, other_statement):
         """
-        Calculate the similarity of two statements based on the closeness of
-        the sentiment value calculated for each statement.
+        Return the similarity of two statements based on
+        their calculated sentiment values.
 
         :return: The percent of similarity between the sentiment value.
         :rtype: float
@@ -206,12 +217,37 @@ class SentimentComparison(Comparator):
 
 
 class JaccardSimilarity(Comparator):
+    """
+    Calculates the similarity of two statements based on the Jaccard index.
+
+    The Jaccard index is composed of a numerator and denominator.
+    In the numerator, we count the number of items that are shared between the sets.
+    In the denominator, we count the total number of items across both sets.
+    Let's say we define sentences to be equivalent if 50% or more of their tokens are equivalent.
+    Here are two sample sentences:
+
+        The young cat is hungry.
+        The cat is very hungry.
+
+    When we parse these sentences to remove stopwords, we end up with the following two sets:
+
+        {young, cat, hungry}
+        {cat, very, hungry}
+
+    In our example above, our intersection is {cat, hungry}, which has count of two.
+    The union of the sets is {young, cat, very, hungry}, which has a count of four.
+    Therefore, our `Jaccard similarity index`_ is two divided by four, or 50%.
+    Given our similarity threshold above, we would consider this to be a match.
+
+    .. _`Jaccard similarity index`: https://en.wikipedia.org/wiki/Jaccard_index
+    """
 
     SIMILARITY_THRESHOLD = 0.5
 
     def initialize_nltk_wordnet(self):
         """
-        Download required NLTK corpora if they have not already been downloaded.
+        Download the NLTK wordnet corpora that is required for this algorithm
+        to run only if the corpora has not already been downloaded.
         """
         from .utils import nltk_download_corpus
 
@@ -219,28 +255,8 @@ class JaccardSimilarity(Comparator):
 
     def compare(self, statement, other_statement):
         """
-        Calculates the similarity of two statements based on the Jaccard index.
-
-        The Jaccard index is composed of a numerator and denominator.
-        In the numerator, we count the number of items that are shared between the sets.
-        In the denominator, we count the total number of items across both sets.
-        Let's say we define sentences to be equivalent if 50% or more of their tokens are equivalent.
-        Here are two sample sentences:
-
-            The young cat is hungry.
-            The cat is very hungry.
-
-        When we parse these sentences to remove stopwords, we end up with the following two sets:
-
-            {young, cat, hungry}
-            {cat, very, hungry}
-
-        In our example above, our intersection is {cat, hungry}, which has count of two.
-        The union of the sets is {young, cat, very, hungry}, which has a count of four.
-        Therefore, our `Jaccard similarity index`_ is two divided by four, or 50%.
-        Given our similarity threshold above, we would consider this to be a match.
-
-        .. _`Jaccard similarity index`: https://en.wikipedia.org/wiki/Jaccard_index
+        Return the calculated similarity of two
+        statements based on the Jaccard index.
         """
         from nltk.corpus import wordnet
         import nltk
@@ -270,10 +286,22 @@ class JaccardSimilarity(Comparator):
         ratio = 0
         pos_a = map(get_wordnet_pos, nltk.pos_tag(nltk.tokenize.word_tokenize(a)))
         pos_b = map(get_wordnet_pos, nltk.pos_tag(nltk.tokenize.word_tokenize(b)))
-        lemma_a = [lemmatizer.lemmatize(token.strip(string.punctuation), pos) for token, pos in pos_a
-                   if pos == wordnet.NOUN and token.strip(string.punctuation) not in stopwords]
-        lemma_b = [lemmatizer.lemmatize(token.strip(string.punctuation), pos) for token, pos in pos_b
-                   if pos == wordnet.NOUN and token.strip(string.punctuation) not in stopwords]
+        lemma_a = [
+            lemmatizer.lemmatize(
+                token.strip(string.punctuation),
+                pos
+            ) for token, pos in pos_a if pos == wordnet.NOUN and token.strip(
+                string.punctuation
+            ) not in stopwords
+        ]
+        lemma_b = [
+            lemmatizer.lemmatize(
+                token.strip(string.punctuation),
+                pos
+            ) for token, pos in pos_b if pos == wordnet.NOUN and token.strip(
+                string.punctuation
+            ) not in stopwords
+        ]
 
         # Calculate Jaccard similarity
         try:
