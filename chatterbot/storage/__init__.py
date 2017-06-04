1e480d12eb4b71dc7321301f42c265c20e83f263
Himanshu Shankar <amhssingh@gmail.com>
2017-06-04 07:55:57 +0530

    Adding ArangoDB Storage Adapter.

--- chatterbot/storage/__init__.py
+++ chatterbot/storage/__init__.py
@@ -1,9 +1,9 @@
-from .storage_adapter import StorageAdapter
+from .arangodb import ArangoStorageAdapter
 from .django_storage import DjangoStorageAdapter
 from .jsonfile import JsonFileStorageAdapter
 from .mongodb import MongoDatabaseAdapter
 from .sqlalchemy_storage import SQLAlchemyDatabaseAdapter
-
+from .storage_adapter import StorageAdapter
 
 __all__ = (
     'StorageAdapter',
@@ -11,4 +11,5 @@ __all__ = (
     'JsonFileStorageAdapter',
     'MongoDatabaseAdapter',
     'SQLAlchemyDatabaseAdapter',
+    'ArangoStorageAdapter',
 )
