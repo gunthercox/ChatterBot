1b5da4cb81bc49db0b8b6decdb1c052ef41cfe81
Gunther Cox <gunthercx@gmail.com>
2017-06-09 07:24:33 -0400

    Add note about possible error

--- chatterbot/trainers.py
+++ chatterbot/trainers.py
@@ -331,6 +331,7 @@ class UbuntuCorpusTrainer(Trainer):
         if sys.version_info[0] > 2:
             # Specify the encoding in Python versions 3 and up
             file_kwargs['encoding'] = 'utf-8'
+            # WARNING: This might fail to read a unicode corpus file in Python 2.x
 
         for file in glob.iglob(extracted_corpus_path):
             self.logger.info('Training from: {}'.format(file))
