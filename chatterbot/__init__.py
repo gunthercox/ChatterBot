6e524a27faa4e33220535610aabfe7020274b3d1
Gunther Cox <gunthercx@gmail.com>
2017-06-24 14:30:35 -0400

    Update release version to 0.6.3

--- chatterbot/__init__.py
+++ chatterbot/__init__.py
@@ -3,7 +3,7 @@ ChatterBot is a machine learning, conversational dialog engine.
 """
 from .chatterbot import ChatBot
 
-__version__ = '0.6.2'
+__version__ = '0.6.3'
 __author__ = 'Gunther Cox'
 __email__ = 'gunthercx@gmail.com'
 __url__ = 'https://github.com/gunthercox/ChatterBot'
