5505fae9390512e5c461810f5cadec225ce36c08
Gunther Cox <gunthercx@gmail.com>
2017-06-27 06:00:05 -0400

    Add utility method for listing nltk data

--- chatterbot/__main__.py
+++ chatterbot/__main__.py
@@ -6,3 +6,8 @@ if __name__ == '__main__':
 
     if '--version' in sys.argv:
         print(chatterbot.__version__)
+
+    if 'list_nltk_data' in sys.argv:
+        import nltk.data
+
+        print('\n'.join(nltk.data.path))
