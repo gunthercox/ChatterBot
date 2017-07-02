fc2a0471d327d1512d2733ebe7f5f07ad7f27e9e
Manish Devgan <manish.nsit8@gmail.com>
2017-07-02 16:43:49 +0100

    flake8 changes made! :D

--- examples/learning_new_response.py
+++ examples/learning_new_response.py
@@ -1,23 +1,23 @@
 from chatterbot import ChatBot
-import logging
+
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+
 # Uncomment the following line to enable verbose logging
+# import logging
 # logging.basicConfig(level=logging.INFO)
 
 # Create a new instance of a ChatBot
 bot = ChatBot("Terminal",
-    storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
-    input_adapter="chatterbot.input.TerminalAdapter",
-    output_adapter="chatterbot.output.TerminalAdapter"
-)
+              storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
+              input_adapter="chatterbot.input.TerminalAdapter",
+              output_adapter="chatterbot.output.TerminalAdapter")
 bot.set_trainer(ChatterBotCorpusTrainer)
 
-bot.train(
-    "chatterbot.corpus.english"
-)
+bot.train("chatterbot.corpus.english")
 
 DEFAULT_SESSION_ID = bot.default_session.id
+
 
 def get_feedback():
     from chatterbot.utils import input_function
@@ -32,6 +32,7 @@ def get_feedback():
         print('Please type either "Yes" or "No"')
         return get_feedback()
 
+
 print("Type something to begin...")
 
 # The following loop will execute each time the user enters input
@@ -40,12 +41,12 @@ while True:
         input_statement = bot.input.process_input_statement()
         statement, response = bot.generate_response(input_statement, DEFAULT_SESSION_ID)
         bot.output.process_response(response)
-        print('\n Is "{}" a coherent response to "{}"? \n'.format(response, input_statement))                      
+        print('\n Is "{}" a coherent response to "{}"? \n'.format(response, input_statement))
         if get_feedback():
             print("please input the correct one")
             response1 = bot.input.process_input_statement()
             bot.learn_response(response1, input_statement)
-            print("Response added to bot!")
+            print("Responses added to bot!")
 
     # Press ctrl-c or ctrl-d on the keyboard to exit
     except (KeyboardInterrupt, EOFError, SystemExit):
