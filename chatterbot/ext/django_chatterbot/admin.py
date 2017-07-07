bb0458dc9f604be1206343f22cfef88cef003bce
Gunther Cox <gunthercx@gmail.com>
2017-07-06 20:20:40 -0400

    Add tags to Django models

--- chatterbot/ext/django_chatterbot/admin.py
+++ chatterbot/ext/django_chatterbot/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from chatterbot.ext.django_chatterbot.models import Statement, Response, Conversation
+from chatterbot.ext.django_chatterbot.models import (
+    Statement, Response, Conversation, Tag
+)
 
 
 class StatementAdmin(admin.ModelAdmin):
@@ -14,9 +16,16 @@ class ResponseAdmin(admin.ModelAdmin):
 
 
 class ConversationAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', )
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    list_filter = ('name', )
+    search_fields = ('name', )
 
 
 admin.site.register(Statement, StatementAdmin)
 admin.site.register(Response, ResponseAdmin)
 admin.site.register(Conversation, ConversationAdmin)
+admin.site.register(Tag, TagAdmin)
