c431cf35c74d61b7723a70b6a3898f181c3d7fe4
Gunther Cox <gunthercx@gmail.com>
2017-07-15 01:09:32 -0400

    Rename session variables

--- chatterbot/chatterbot.py
+++ chatterbot/chatterbot.py
@@ -75,7 +75,7 @@ class ChatBot(object):
         self.trainer = TrainerClass(self.storage, **kwargs)
         self.training_data = kwargs.get('training_data')
 
-        self.default_session_id = None
+        self.default_conversation_id = None
 
         self.logger = kwargs.get('logger', logging.getLogger(__name__))
 
@@ -91,7 +91,7 @@ class ChatBot(object):
         """
         self.logic.initialize()
 
-    def get_response(self, input_item, session_id=None):
+    def get_response(self, input_item, conversation_id=None):
         """
         Return the bot's response based on the input.
 
@@ -99,10 +99,10 @@ class ChatBot(object):
         :returns: A response to the input.
         :rtype: Statement
         """
-        if not session_id:
-            if not self.default_session_id:
-                self.default_session_id = self.storage.create_conversation()
-            session_id = self.default_session_id
+        if not conversation_id:
+            if not self.default_conversation_id:
+                self.default_conversation_id = self.storage.create_conversation()
+            conversation_id = self.default_conversation_id
 
         input_statement = self.input.process_input_statement(input_item)
 
@@ -110,23 +110,23 @@ class ChatBot(object):
         for preprocessor in self.preprocessors:
             input_statement = preprocessor(self, input_statement)
 
-        statement, response = self.generate_response(input_statement, session_id)
+        statement, response = self.generate_response(input_statement, conversation_id)
 
         # Learn that the user's input was a valid response to the chat bot's previous output
-        previous_statement = self.storage.get_latest_response(session_id)
+        previous_statement = self.storage.get_latest_response(conversation_id)
 
         if not self.read_only:
             self.learn_response(statement, previous_statement)
-            self.storage.add_to_converation(session_id, statement, response)
+            self.storage.add_to_converation(conversation_id, statement, response)
 
         # Process the response output with the output adapter
-        return self.output.process_response(response, session_id)
+        return self.output.process_response(response, conversation_id)
 
-    def generate_response(self, input_statement, session_id):
+    def generate_response(self, input_statement, conversation_id):
         """
         Return a response based on a given input statement.
         """
-        self.storage.generate_base_query(self, session_id)
+        self.storage.generate_base_query(self, conversation_id)
 
         # Select a response to the input statement
         response = self.logic.process(input_statement)
