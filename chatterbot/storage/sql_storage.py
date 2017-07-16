f7d5ab4b728b44a744f7d40f81827246f2e4205f
Gunther Cox <gunthercx@gmail.com>
2017-07-16 10:32:39 -0400

    Maybe fix sql error

--- chatterbot/storage/sql_storage.py
+++ chatterbot/storage/sql_storage.py
@@ -9,10 +9,11 @@ Base = None
 
 try:
     from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy_mixins import ActiveRecordMixin, SmartQueryMixin
 
     Base = declarative_base()
 
-    class StatementTable(Base):
+    class StatementTable(Base, ActiveRecordMixin, SmartQueryMixin):
         """
         StatementTable, placeholder for a sentence or phrase.
         """
@@ -46,7 +47,7 @@ try:
             default=get_statement_serialized
         )
 
-    class ResponseTable(Base):
+    class ResponseTable(Base, ActiveRecordMixin, SmartQueryMixin):
         """
         ResponseTable, contains responses related to a givem statment.
         """
@@ -63,7 +64,7 @@ try:
 
         id = Column(Integer)
         text = Column(String, primary_key=True)
-        occurrence = Column(Integer)
+        occurrence = Column(Integer, default=1)
         statement_text = Column(String, ForeignKey('StatementTable.text'))
 
         statement_table = relationship(
@@ -84,6 +85,19 @@ try:
 
 except ImportError:
     pass
+
+
+def get_or_create(session, model, defaults=None, **kwargs):
+    from sqlalchemy.sql.expression import ClauseElement
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance, False
+    else:
+        params = dict((k, v) for k, v in kwargs.iteritems() if not isinstance(v, ClauseElement))
+        params.update(defaults or {})
+        instance = model(**params)
+        session.add(instance)
+        return instance, True
 
 
 def get_statement_table(statement):
@@ -271,7 +285,9 @@ class SQLStorageAdapter(StorageAdapter):
         """
         if statement:
             session = self.Session()
+            ResponseTable.set_session(session)
             query = self.__statement_filter(session, **{"text": statement.text})
+            # TODO: This should be a get
             record = query.first()
 
             if record:
@@ -281,9 +297,25 @@ class SQLStorageAdapter(StorageAdapter):
                 if statement.extra_data:
                     record.extra_data = dict(statement.extra_data)
                 if statement.in_response_to:
-                    record.in_response_to = list(
-                        map(get_response_table, statement.in_response_to)
-                    )
+
+                    responses = []
+
+                    # Get or create the response records as needed
+                    for response in statement.in_response_to:
+                        _response = ResponseTable.where(
+                            text=statement.text,
+                            statement_text=response.text
+                        ).first()
+
+                        if _response:
+                            _response.occurrence += 1
+                        else:
+                            # Create the record
+                            _response = get_response_table(response)
+
+                        responses.append(_response)
+
+                    record.in_response_to = responses
                 session.add(record)
             else:
                 session.add(get_statement_table(statement))
