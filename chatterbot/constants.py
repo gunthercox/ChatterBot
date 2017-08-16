332c2c6d347b800e8ef4a523a5c327f6a2b230ba
Gunther Cox <gunthercx@gmail.com>
2017-08-15 20:38:24 -0400

    Add constant for maximum statement text length

---
+++ chatterbot/constants.py
@@ -0,0 +1,10 @@
+"""
+ChatterBot constants
+"""
+
+'''
+The maximum length of characters that the text of a statement can contain.
+This should be enforced on a per-model basis by the data model for each
+storage adapter.
+'''
+MAXIMUM_DIALOG_CHARACTERS = 400
