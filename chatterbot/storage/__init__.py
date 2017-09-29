55f49066cedef4972c0b8e7ecbabafa44ff5d895
Gunther Cox <gunthercx@gmail.com>
2017-09-29 06:58:41 -0400

    Remove JSON file storage adapter

--- chatterbot/storage/__init__.py
+++ chatterbot/storage/__init__.py
@@ -1,6 +1,5 @@
 from .storage_adapter import StorageAdapter
 from .django_storage import DjangoStorageAdapter
-from .jsonfile import JsonFileStorageAdapter
 from .mongodb import MongoDatabaseAdapter
 from .sql_storage import SQLStorageAdapter
 
@@ -8,7 +7,6 @@ from .sql_storage import SQLStorageAdapter
 __all__ = (
     'StorageAdapter',
     'DjangoStorageAdapter',
-    'JsonFileStorageAdapter',
     'MongoDatabaseAdapter',
     'SQLStorageAdapter',
 )
