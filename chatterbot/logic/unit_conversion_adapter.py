78c050446266b473aab284f5303050e36f60ecd6
manoelrui <manoelrui19@gmail.com>
2018-05-08 21:14:29 -0300

    Added unit test to convert inchs to kilometer

--- chatterbot/logic/unit_conversion_adapter.py
+++ chatterbot/logic/unit_conversion_adapter.py
@@ -1,4 +1,5 @@
 from chatterbot.logic import LogicAdapter
+from chatterbot.conversation import Statement
 import re
 
 
@@ -11,10 +12,28 @@ class UnitConversion(LogicAdapter):
             'eighteen|nineteen|twenty|thirty|forty|fifty|sixty|seventy|'
             'eighty|ninety|hundred|thousand)'
         )
-        self.pattern = r'''([Hh]ow many)(\s+)(?P<from>\S+)(\s+)((are)*\s+in)(\s+)(?P<number>\d+|(%s[-\s]?)+)(\s+)(?P<target>\S+)\?''' % self.number
+        self.pattern = r'''([Hh]ow many)(\s+)(?P<target>\S+)(\s+)((are)*\s+in)(\s+)(?P<number>\d+|(%s[-\s]?)+)(\s+)(?P<from>\S+)\?''' % self.number
+
+    def get_unit(self, text):
+        pass
 
     def can_process(self, statment):
         return re.match(self.pattern, statment.text)
 
     def process(self, statment):
-        pass
+        from pint import UnitRegistry
+        from mathparse import mathparse
+
+        response = Statement(text='')
+        p = re.match(self.pattern, statment.text)
+        if p is None:
+            response.confidence = 0
+            return response
+
+        n = mathparse.parse(p.group("number"), "ENG")
+
+        ureg = UnitRegistry()
+        from_unit_pint = float(n) * ureg.kilometer
+        target_unit_pint = from_unit_pint.to(ureg.inch)
+        response.text = target_unit_pint.magnitude
+        return response
--- tests/logic_adapter_tests/test_unit_conversion.py
+++ tests/logic_adapter_tests/test_unit_conversion.py
@@ -15,3 +15,9 @@ class UnitConversionTests(TestCase):
     def test_can_not_process(self):
         statement = Statement('What is love?')
         self.assertFalse(self.adapter.can_process(statement))
+
+    def test_inches_to_kilometers(self):
+        statement = Statement('How many inches are in two kilometers?')
+        expected_value = 78740.2
+        returned_value = float(self.adapter.process(statement).text)
+        self.assertLessEqual(returned_value - expected_value, 1e-10)
