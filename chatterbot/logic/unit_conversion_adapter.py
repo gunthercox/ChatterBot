3031799c359701f54e03f48741b3f705475f34f1
manoelrui <manoelrui19@gmail.com>
2018-05-09 21:55:59 -0300

    Improved unit conversion logic

--- chatterbot/logic/unit_conversion_adapter.py
+++ chatterbot/logic/unit_conversion_adapter.py
@@ -3,6 +3,11 @@ from chatterbot.conversation import Statement
 import re
 
 
+supported_units = ['kilometer', 'hectometer', 'decameter', 'decimeter',
+        'centimeter', 'millimeter', 'micrometer', 'nanometer',
+        'picometer', 'femtometer', 'attometer', 'inches']
+
+
 class UnitConversion(LogicAdapter):
     def __init__(self, **kwargs):
         super(UnitConversion, self).__init__(**kwargs)
@@ -23,17 +28,45 @@ class UnitConversion(LogicAdapter):
     def process(self, statment):
         from pint import UnitRegistry
         from mathparse import mathparse
+        # Use python-Levenshtein if available
+        try:
+            from Levenshtein.StringMatcher import StringMatcher as SequenceMatcher
+        except ImportError:
+            from difflib import SequenceMatcher
 
         response = Statement(text='')
+        response.confidence = 1
+
         p = re.match(self.pattern, statment.text)
         if p is None:
             response.confidence = 0
             return response
 
-        n = mathparse.parse(p.group("number"), "ENG")
+        try:
+            n = mathparse.parse(p.group("number"), "ENG")
+            target_parsed = p.group("target")
+            from_parsed = p.group("from")
 
-        ureg = UnitRegistry()
-        from_unit_pint = float(n) * ureg.kilometer
-        target_unit_pint = from_unit_pint.to(ureg.inch)
-        response.text = target_unit_pint.magnitude
-        return response
+            target_unit = [None, 0.0]
+            from_unit = [None, 0.0]
+            for unit in supported_units:
+                target_ratio = SequenceMatcher(None, unit, target_parsed).ratio()
+                from_ratio = SequenceMatcher(None, unit, from_parsed).ratio()
+                if target_ratio >= target_unit[1]:
+                    target_unit[0] = unit
+                    target_unit[1] = target_ratio
+                if SequenceMatcher(None, unit, from_parsed).ratio() >= from_unit[1]:
+                    from_unit[0] = unit
+                    from_unit[1] = from_ratio
+
+            ureg = UnitRegistry()
+            from_value = float(n) * getattr(ureg, from_unit[0])
+            target_value = from_value.to(getattr(ureg, target_unit[0]))
+            response.text = target_value.magnitude
+
+        except Exception, e:
+            response.confidence = 0
+
+        finally:
+            return response
+
