d533696c0513611dc7b97b76a6f08d8ab576200f
manoelrui <manoel.rui@lsbd.ufc.br>
2018-05-11 11:47:34 -0300

    Added unit test test_meter_to_kilometer

--- tests/logic_adapter_tests/test_unit_conversion.py
+++ tests/logic_adapter_tests/test_unit_conversion.py
@@ -16,18 +16,18 @@ class UnitConversionTests(TestCase):
         statement = Statement('What is love?')
         self.assertFalse(self.adapter.can_process(statement))
 
+    def test_inches_to_kilometers(self):
+        statement = Statement('How many inches are in two kilometers?')
+        expected_value = 78740.2
+        response_statement = self.adapter.process(statement)
+        self.assertLessEqual(abs(response_statement.confidence - 1.0), 1e-10)
+        self.assertIsNotNone(response_statement)
+        self.assertLessEqual(abs(float(response_statement.text) - expected_value), 0.1)
+
     def test_meter_to_kilometer(self):
         statement = Statement('How many meters are in one kilometer?')
         expected_value = 1000
         response_statement = self.adapter.process(statement)
-        self.assertGreaterEqual(response_statement.confidence, 0.9)
+        self.assertLessEqual(abs(response_statement.confidence - 1.0), 0.1)
         self.assertIsNotNone(response_statement)
-        self.assertLessEqual(float(response_statement.text) - expected_value, 1e-10)
-
-    def test_inches_in_two_kilometers(self):
-        statement = Statement('How many inches are in two kilometers?')
-        expected_value = 78740.2
-        response_statement = self.adapter.process(statement)
-        self.assertEqual(response_statement.confidence, 1)
-        self.assertIsNotNone(response_statement)
-        self.assertLessEqual(float(response_statement.text) - expected_value, 1e-10)
+        self.assertLessEqual(abs(float(response_statement.text) - expected_value), 0.1)
