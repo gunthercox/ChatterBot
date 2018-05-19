0a83530731992101faaf1569e630ea26b0bbd962
manoelrui <manoelrui19@gmail.com>
2018-05-19 20:55:05 -0300

    Added support to anther type of units

--- chatterbot/logic/unit_conversion_adapter.py
+++ chatterbot/logic/unit_conversion_adapter.py
@@ -4,23 +4,15 @@ from chatterbot import parsing
 import re
 
 
-supported_units = ['meter', 'kilometer', 'hectometer', 'decameter', 'decimeter',
-                   'centimeter', 'millimeter', 'micrometer', 'nanometer',
-                   'picometer', 'femtometer', 'attometer', 'inches']
-
-
 class UnitConversion(LogicAdapter):
     def __init__(self, **kwargs):
         super(UnitConversion, self).__init__(**kwargs)
         self.pattern = r'''
-                       ([Hh]ow[ ]many)
-                       \s+
-                       (?P<target>\S+) # meter, kilometer, hectometer
-                       \s+
-                       ((are)*\s*in)
-                       \s+
-                       (?P<number>\d+|(a|an)|(%s[-\s]?)+)(\s+)
-                       (?P<from>\S+)\s*\? # meter, kilometer, hectometer
+                       (([Hh]ow\s+many)\s+
+                       (?P<target>\S+)\s+ # meter, kilometer, hectometer
+                       ((are)*\s*in)\s+
+                       (?P<number>\d+|(a|an)|(%s[-\s]?)+)\s+
+                       (?P<from>\S+)\s*\?) # meter, kilometer, hectometer
                        ''' % (parsing.numbers)
 
     def can_process(self, statment):
@@ -30,11 +22,6 @@ class UnitConversion(LogicAdapter):
     def process(self, statment):
         from pint import UnitRegistry
         from mathparse import mathparse
-        # Use python-Levenshtein if available
-        try:
-            from Levenshtein.StringMatcher import StringMatcher as SequenceMatcher
-        except ImportError:
-            from difflib import SequenceMatcher
 
         response = Statement(text='')
         response.confidence = 1
@@ -57,23 +44,9 @@ class UnitConversion(LogicAdapter):
                 else:
                     raise
 
-            target_unit = [None, 0.0]
-            from_unit = [None, 0.0]
-            for unit in supported_units:
-                target_ratio = SequenceMatcher(None, unit, target_parsed).ratio()
-                from_ratio = SequenceMatcher(None, unit, from_parsed).ratio()
-                if target_ratio >= target_unit[1]:
-                    target_unit[0] = unit
-                    target_unit[1] = target_ratio
-                if SequenceMatcher(None, unit, from_parsed).ratio() >= from_unit[1]:
-                    from_unit[0] = unit
-                    from_unit[1] = from_ratio
-
-            response.confidence = (target_unit[1] + target_unit[1]) / 2.0
-
             ureg = UnitRegistry()
-            from_value = float(n) * getattr(ureg, from_unit[0])
-            target_value = from_value.to(getattr(ureg, target_unit[0]))
+            from_value = ureg.Quantity(float(n), getattr(ureg, from_parsed))
+            target_value = from_value.to(getattr(ureg, target_parsed))
             response.text = target_value.magnitude
 
         except Exception:
