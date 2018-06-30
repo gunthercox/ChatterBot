377b4ff56da5f9420a6ab6dd1c5a1d82b1bd832f
Gunther Cox <gunthercx@gmail.com>
2018-06-30 12:25:05 -0400

    Update python packaging language versions

--- setup.py
+++ setup.py
@@ -64,11 +64,12 @@ setup(
         'Topic :: Communications :: Chat',
         'Topic :: Internet',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
     ],
     test_suite='tests'
 )
