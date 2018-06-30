48d3e9d7e8159fb2d4677d2effbdc5990ee8316f
Gunther Cox <gunthercx@gmail.com>
2018-06-30 12:32:27 -0400

    Remove url inlude for Django 2.0

--- examples/django_app/example_app/urls.py
+++ examples/django_app/example_app/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 from example_app.views import ChatterBotAppView, ChatterBotApiView
 
 
 urlpatterns = [
     url(r'^$', ChatterBotAppView.as_view(), name='main'),
-    url(r'^admin/', include(admin.site.urls), name='admin'),
+    url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^api/chatterbot/', ChatterBotApiView.as_view(), name='chatterbot'),
 ]
