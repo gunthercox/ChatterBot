ab90f2be3929b43058e213787a3a1b9cf4a306e4
Gunther Cox <gunthercx@gmail.com>
2018-08-07 20:05:16 -0400

    Remove test case for response objects

--- tests/conversation_tests/test_responses.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from chatterbot.conversation import Response
-
-
-class ResponseTests(TestCase):
-
-    def setUp(self):
-        self.response = Response("A test response.")
