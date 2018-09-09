2a96c11feeb5093c09726bbf1398159464a6d805
Gunther Cox <gunthercx@gmail.com>
2018-09-09 17:34:55 -0400

    Revert "Add creation time to Django admin"
    This reverts commit f7a827aba0eb5542f0b41c8c897d8314095317ef.

--- chatterbot/ext/django_chatterbot/admin.py
+++ chatterbot/ext/django_chatterbot/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from chatterbot.ext.django_chatterbot.models import Statement, Response, Conversation, Tag
+from chatterbot.ext.django_chatterbot.models import (
+    Statement, Response, Conversation, Tag
+)
 
 
 class StatementAdmin(admin.ModelAdmin):
-    list_display = ('text', 'created_at', )
-    list_filter = ('text', 'created_at', )
+    list_display = ('text', )
+    list_filter = ('text', )
     search_fields = ('text', )
 
 
