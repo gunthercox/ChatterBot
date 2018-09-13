b898d347ab0c437cd87c934504328533cee43f2e
Gunther Cox <gunthercx@gmail.com>
2018-09-12 20:41:48 -0400

    Log Django test output to stdout

--- tests_django/test_settings.py
+++ tests_django/test_settings.py
@@ -58,3 +58,19 @@ PASSWORD_HASHERS = (
 )
 
 USE_TZ = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
