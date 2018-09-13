13b1045af1d16f8a4a8cbf081bd92bfac9fe4676
Gunther Cox <gunthercx@gmail.com>
2018-09-12 20:03:09 -0400

    Add CONVERSATION_LABEL_MAX_LENGTH constant

--- chatterbot/constants.py
+++ chatterbot/constants.py
@@ -9,6 +9,13 @@ storage adapter.
 '''
 STATEMENT_TEXT_MAX_LENGTH = 400
 
+'''
+The maximum length of characters that the text label of a conversation can contain.
+The number 32 was chosen because that is the length of the string representation
+of a UUID4 with no hyphens.
+'''
+CONVERSATION_LABEL_MAX_LENGTH = 32
+
 # The maximum length of characters that the name of a tag can contain
 TAG_NAME_MAX_LENGTH = 50
 
