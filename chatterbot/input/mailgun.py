3c41f4ec07a0bf04f145facf6ec71491f038b640
Gunther Cox <gunthercx@gmail.com>
2018-09-25 21:35:52 -0400

    Create common module for mailgun API methods

--- chatterbot/input/mailgun.py
+++ chatterbot/input/mailgun.py
@@ -1,6 +1,6 @@
-import datetime
 from chatterbot.input import InputAdapter
 from chatterbot.conversation import Statement
+from chatterbot.api import mailgun
 
 
 class Mailgun(InputAdapter):
@@ -9,7 +9,7 @@ class Mailgun(InputAdapter):
     """
 
     def __init__(self, **kwargs):
-        super(Mailgun, self).__init__(**kwargs)
+        super().__init__(**kwargs)
 
         # Use the bot's name for the name of the sender
         self.name = kwargs.get('name')
@@ -17,42 +17,11 @@ class Mailgun(InputAdapter):
         self.api_key = kwargs.get('mailgun_api_key')
         self.endpoint = kwargs.get('mailgun_api_endpoint')
 
-    def get_email_stored_events(self):
-        import requests
-
-        yesterday = datetime.datetime.now() - datetime.timedelta(1)
-        return requests.get(
-            '{}/events'.format(self.endpoint),
-            auth=('api', self.api_key),
-            params={
-                'begin': yesterday.isoformat(),
-                'ascending': 'yes',
-                'limit': 1
-            }
-        )
-
-    def get_stored_email_urls(self):
-        response = self.get_email_stored_events()
-        data = response.json()
-
-        for item in data.get('items', []):
-            if 'storage' in item:
-                if 'url' in item['storage']:
-                    yield item['storage']['url']
-
-    def get_message(self, url):
-        import requests
-
-        return requests.get(
-            url,
-            auth=('api', self.api_key)
-        )
-
     def process_input(self, statement):
-        urls = self.get_stored_email_urls()
+        urls = mailgun.get_stored_email_urls(self.api_key, self.endpoint)
         url = list(urls)[0]
 
-        response = self.get_message(url)
+        response = mailgun.get_message(self.api_key, url)
         message = response.json()
 
         text = message.get('stripped-text')
