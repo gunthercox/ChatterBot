0eb0d500efccc7b8adb8e65dc35ab45d7ee663bf
Gunther Cox <gunthercx@gmail.com>
2018-10-04 07:52:04 -0400

    Add additional fields for ordinals

--- chatterbot/parsing.py
+++ chatterbot/parsing.py
@@ -695,11 +695,28 @@ HASHWEEKDAYS = {
 
 # Ordinal to number
 HASHORDINALS = {
+    'zeroth': 0,
     'first': 1,
     'second': 2,
     'third': 3,
     'fourth': 4,
     'forth': 4,
+    'fifth': 5,
+    'sixth': 6,
+    'seventh': 7,
+    'eighth': 8,
+    'ninth': 9,
+    'tenth': 10,
+    'eleventh': 11,
+    'twelfth': 12,
+    'thirteenth': 13,
+    'fourteenth': 14,
+    'fifteenth': 15,
+    'sixteenth': 16,
+    'seventeenth': 17,
+    'eighteenth': 18,
+    'nineteenth': 19,
+    'twentieth': 20,
     'last': -1
 }
 
