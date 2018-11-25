3b5c2767755b46088897df257bacf5957252f203
Gunther Cox <gunthercx@gmail.com>
2018-11-25 11:19:49 -0500

    Allow statements containing specific words to be excluded from response results.

--- tests/logic/test_best_match.py
+++ tests/logic/test_best_match.py
@@ -1,4 +1,3 @@
-from unittest.mock import MagicMock
 from chatterbot.logic import BestMatch
 from chatterbot.conversation import Statement
 from tests.base_case import ChatBotTestCase
@@ -17,11 +16,32 @@ class BestMatchTestCase(ChatBotTestCase):
         """
         An exception should be raised if there is no data in the database.
         """
-        self.adapter.chatbot.storage.filter = MagicMock(return_value=[])
-        self.adapter.chatbot.storage.count = MagicMock(return_value=0)
-
         statement = Statement(text='What is your quest?')
         response = self.adapter.get(statement)
 
         self.assertEqual(response.text, 'What is your quest?')
         self.assertEqual(response.confidence, 0)
+
+    def test_excluded_words(self):
+        """
+        Test that the logic adapter cannot return a response containing
+        any of the listed words for exclusion.
+        """
+        self.chatbot.storage.create(
+            text='I like to count.'
+        )
+        self.chatbot.storage.create(
+            text='Counting is dumb.',
+            in_response_to='I like to count.'
+        )
+        self.chatbot.storage.create(
+            text='Counting is fun!',
+            in_response_to='I like to count.'
+        )
+
+        self.adapter.excluded_words = ['dumb']
+
+        response = self.adapter.process(Statement(text='I like to count.'))
+
+        self.assertEqual(response.confidence, 1)
+        self.assertEqual(response.text, 'Counting is fun!')
