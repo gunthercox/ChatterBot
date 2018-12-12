ac15891a839af62c0b55b658ce8609387cfaa9e3
Martin Delille <martin@phonations.com>
2018-12-12 12:46:31 +0100

    basic example fix #1426

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -1,16 +1,14 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
 # Create a new chat bot named Charlie
 chatbot = ChatBot('Charlie')
 
-trainer = ListTrainer(chatbot)
+# Create a new trainer for the chatbox
+trainer = ChatterBotCorpusTrainer(chatbot.storage)
 
-trainer.train([
-    "Hi, can I help you?",
-    "Sure, I'd like to book a flight to Iceland.",
-    "Your flight has been booked."
-])
+# Train the chatbot based on the english corpus
+trainer.train("chatterbot.corpus.english");
 
 # Get a response to the input text 'I would like to book a flight.'
 response = chatbot.get_response('I would like to book a flight.')
