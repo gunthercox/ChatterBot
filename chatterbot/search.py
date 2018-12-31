b8f49ce8b4eca30341952394b54c5dc760727fda
Gunther Cox <gunthercx@gmail.com>
2018-12-31 14:59:10 -0500

    Create search module

---
+++ chatterbot/search.py
@@ -0,0 +1,69 @@
+class Search:
+    """
+    :param statement_comparison_function: The dot-notated import path
+        to a statement comparison function.
+        Defaults to ``levenshtein_distance``.
+
+    :param search_page_size:
+        The maximum number of records to load into memory at a time when searching.
+        Defaults to 1000
+    """
+
+    name = 'search'
+
+    def __init__(self, chatbot, **kwargs):
+        from chatterbot.comparisons import levenshtein_distance
+
+        self.chatbot = chatbot
+
+        self.compare_statements = kwargs.get(
+            'statement_comparison_function',
+            levenshtein_distance
+        )
+
+        self.search_page_size = kwargs.get(
+            'search_page_size', 1000
+        )
+
+    def search(self, input_statement):
+        """
+        Search for close matches to the input. Confidence scores for
+        subsequent results will order of increasing value.
+
+        :param input_statement: A statement.
+        :type input_statement: chatterbot.conversation.Statement
+        :rtype: Generator yielding one closest matching statement at a time.
+        """
+        self.chatbot.logger.info('Beginning search for close text match')
+
+        # TODO: Use input_statement.search_text if already generated
+        #   Maybe do this ahead of time (when the chatbot gets the input)
+        #   and just put a warning here if the input statement has no search text.
+        input_search_text = self.chatbot.storage.tagger.get_bigram_pair_string(
+            input_statement.text
+        )
+
+        statement_list = self.chatbot.storage.filter(
+            search_text_contains=input_search_text,
+            persona_not_startswith='bot:',
+            page_size=self.search_page_size
+        )
+
+        closest_match = input_statement
+        closest_match.confidence = 0
+
+        self.chatbot.logger.info('Processing search results')
+
+        # Find the closest matching known statement
+        for statement in statement_list:
+            confidence = self.compare_statements(input_statement, statement)
+
+            if confidence > closest_match.confidence:
+                statement.confidence = confidence
+                closest_match = statement
+
+                self.chatbot.logger.info('Similar text found: {} {}'.format(
+                    closest_match.text, confidence
+                ))
+
+                yield closest_match
---
+++ tests/test_search.py
@@ -0,0 +1,191 @@
+from tests.base_case import ChatBotTestCase
+from chatterbot.conversation import Statement
+from chatterbot.search import Search
+from chatterbot import comparisons
+
+
+class SearchTestCase(ChatBotTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.search_algorithm = Search(self.chatbot)
+
+    def test_search_no_results(self):
+        """
+        An exception should be raised if there is no data to return.
+        """
+        statement = Statement(text='What is your quest?')
+
+        with self.assertRaises(StopIteration):
+            next(self.search_algorithm.search(statement))
+
+    def test_search_cast_to_list_no_results(self):
+        """
+        An empty list should be returned when the generator is
+        cast to a list and there are no results to return.
+        """
+        statement = Statement(text='What is your quest?')
+
+        results = list(self.search_algorithm.search(statement))
+
+        self.assertEqual(results, [])
+
+
+class SearchComparisonFunctionSynsetDistanceTests(ChatBotTestCase):
+    """
+    Test that the search algorithm works correctly with the
+    synset distance comparison function.
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.search_algorithm = Search(
+            self.chatbot,
+            statement_comparison_function=comparisons.synset_distance
+        )
+
+    def test_get_closest_statement(self):
+        """
+        Note, the content of the in_response_to field for each of the
+        test statements is only required because the logic adapter will
+        filter out any statements that are not in response to a known statement.
+        """
+        self.chatbot.storage.create_many([
+            Statement(text='This is a lovely bog.', in_response_to='This is a lovely bog.'),
+            Statement(text='This is a beautiful swamp.', in_response_to='This is a beautiful swamp.'),
+            Statement(text='It smells like a swamp.', in_response_to='It smells like a swamp.')
+        ])
+
+        statement = Statement(text='This is a lovely swamp.')
+        results = list(self.search_algorithm.search(statement))
+
+        self.assertIsLength(results, 1)
+        self.assertEqual(results[0], 'This is a lovely bog.')
+        self.assertGreater(results[0].confidence, 0)
+
+    def test_different_punctuation(self):
+        self.chatbot.storage.create_many([
+            Statement(text='Who are you?'),
+            Statement(text='Are you good?'),
+            Statement(text='You are good')
+        ])
+
+        statement = Statement(text='Are you good')
+        results = list(self.search_algorithm.search(statement))
+
+        self.assertIsLength(results, 1)
+        self.assertEqual(results[0], 'Are you good?')
+
+
+class SearchComparisonFunctionSentimentComparisonTests(ChatBotTestCase):
+    """
+    Test that the search algorithm works correctly with the
+    sentiment comparison function by using the similarity
+    of sentiment polarity.
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.search_algorithm = Search(
+            self.chatbot,
+            statement_comparison_function=comparisons.sentiment_comparison
+        )
+
+    def test_exact_input(self):
+        self.chatbot.storage.create(text='What is your favorite flavor of ice cream?')
+        self.chatbot.storage.create(text='I enjoy raspberry ice cream.')
+        self.chatbot.storage.create(text='I am glad to hear that.')
+        self.chatbot.storage.create(text='Thank you.')
+
+        happy_statement = Statement(text='I enjoy raspberry ice cream.')
+        results = list(self.search_algorithm.search(happy_statement))
+
+        self.assertIsLength(results, 1)
+        self.assertEqual(results[0].text, 'I enjoy raspberry ice cream.')
+        self.assertEqual(results[0].confidence, 1)
+
+    def test_close_input(self):
+        self.chatbot.storage.create(text='What is your favorite flavor of ice cream?')
+        self.chatbot.storage.create(text='I enjoy raspberry ice cream.')
+        self.chatbot.storage.create(text='I am glad to hear that.')
+        self.chatbot.storage.create(text='Thank you, what is yours?')
+        self.chatbot.storage.create(text='Mine is chocolate.')
+
+        happy_statement = Statement(text='I enjoy raspberry.')
+        results = list(self.search_algorithm.search(happy_statement))
+
+        self.assertIsLength(results, 1)
+        self.assertEqual(results[0].text, 'I enjoy raspberry ice cream.')
+        self.assertAlmostEqual(results[0].confidence, 0.75, places=1)
+
+
+class SearchComparisonFunctionLevenshteinDistanceComparisonTests(ChatBotTestCase):
+    """
+    Test that the search algorithm works correctly with the
+    Levenshtein distance comparison function.
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.search_algorithm = Search(
+            self.chatbot,
+            statement_comparison_function=comparisons.levenshtein_distance
+        )
+
+    def test_get_closest_statement(self):
+        """
+        Note, the content of the in_response_to field for each of the
+        test statements is only required because the logic adapter will
+        filter out any statements that are not in response to a known statement.
+        """
+        self.chatbot.storage.create_many([
+            Statement(text='Who do you love?', in_response_to='I hear you are going on a quest?'),
+            Statement(text='What is the meaning of life?', in_response_to='Yuck, black licorice jelly beans.'),
+            Statement(text='I am Iron Man.', in_response_to='What... is your quest?'),
+            Statement(text='What... is your quest?', in_response_to='I am Iron Man.'),
+            Statement(text='Yuck, black licorice jelly beans.', in_response_to='What is the meaning of life?'),
+            Statement(text='I hear you are going on a quest?', in_response_to='Who do you love?'),
+        ])
+
+        statement = Statement(text='What is your quest?')
+
+        results = list(self.search_algorithm.search(statement))
+
+        self.assertIsLength(results, 1)
+        self.assertEqual(results[0], 'What... is your quest?')
+
+    def test_confidence_exact_match(self):
+        self.chatbot.storage.create(text='What is your quest?', in_response_to='What is your quest?')
+
+        statement = Statement(text='What is your quest?')
+        results = list(self.search_algorithm.search(statement))
+
+        self.assertIsLength(results, 1)
+        self.assertEqual(results[0].confidence, 1)
+
+    def test_confidence_half_match(self):
+        from unittest.mock import MagicMock
+
+        # Assume that the storage adapter returns a partial match
+        self.chatbot.storage.filter = MagicMock(return_value=[
+            Statement(text='xxyy')
+        ])
+
+        statement = Statement(text='wwxx')
+        results = list(self.search_algorithm.search(statement))
+
+        self.assertIsLength(results, 1)
+        self.assertEqual(results[0].confidence, 0.5)
+
+    def test_confidence_no_match(self):
+        from unittest.mock import MagicMock
+
+        # Assume that the storage adapter returns a partial match
+        self.search_algorithm.chatbot.storage.filter = MagicMock(return_value=[
+            Statement(text='xxx', in_response_to='xxx')
+        ])
+
+        statement = Statement(text='yyy')
+        results = list(self.search_algorithm.search(statement))
+
+        self.assertIsLength(results, 0)
