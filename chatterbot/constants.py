58fc39ae95522ce152b4ff137071f74c5490e14e
Gunther Cox <gunthercx@gmail.com>
2019-01-12 11:16:33 -0500

    Change statement text max-length to 255

--- chatterbot/constants.py
+++ chatterbot/constants.py
@@ -4,10 +4,11 @@ ChatterBot constants
 
 '''
 The maximum length of characters that the text of a statement can contain.
-This should be enforced on a per-model basis by the data model for each
-storage adapter.
+The number 255 is used because that is the maximum length of a char field
+in most databases. This value should be enforced on a per-model basis by
+the data model for each storage adapter.
 '''
-STATEMENT_TEXT_MAX_LENGTH = 400
+STATEMENT_TEXT_MAX_LENGTH = 255
 
 '''
 The maximum length of characters that the text label of a conversation can contain.
