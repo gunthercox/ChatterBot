dff095b70d61490f44d5e202a0d3de51588eba6f
Gunther Cox <gunthercx@gmail.com>
2019-01-20 09:37:07 -0500

    Move Django admin classes to their own file

--- chatterbot/ext/django_chatterbot/admin.py
+++ chatterbot/ext/django_chatterbot/admin.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
+from chatterbot.ext.django_chatterbot.model_admin import StatementAdmin, TagAdmin
 from chatterbot.ext.django_chatterbot.models import Statement, Tag
-
-
-class StatementAdmin(admin.ModelAdmin):
-    list_display = ('text', 'in_response_to', 'conversation', 'created_at', )
-    list_filter = ('text', 'created_at', )
-    search_fields = ('text', )
-
-
-class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', )
-    list_filter = ('name', )
-    search_fields = ('name', )
 
 
 admin.site.register(Statement, StatementAdmin)
