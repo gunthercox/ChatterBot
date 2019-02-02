0326f5afd7d10b44f7ca5408be595f44883d3c8c
Gunther Cox <gunthercx@gmail.com>
2019-02-02 10:03:18 -0500

    Organize training tests

--- tests/training/test_training.py
+++ tests/training/test_training.py
@@ -1,5 +1,6 @@
 from tests.base_case import ChatBotTestCase
 from chatterbot.trainers import Trainer
+from chatterbot.conversation import Statement
 
 
 class TrainingTests(ChatBotTestCase):
@@ -12,3 +13,14 @@ class TrainingTests(ChatBotTestCase):
     def test_trainer_not_set(self):
         with self.assertRaises(Trainer.TrainerInitializationException):
             self.trainer.train()
+
+    def test_generate_export_data(self):
+        self.chatbot.storage.create_many([
+            Statement(text='Hello, how are you?'),
+            Statement(text='I am good.', in_response_to='Hello, how are you?')
+        ])
+        data = self.trainer._generate_export_data()
+
+        self.assertEqual(
+            [['Hello, how are you?', 'I am good.']], data
+        )
