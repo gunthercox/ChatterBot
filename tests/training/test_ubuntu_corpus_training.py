4cfb7d9cb1282a88c9a67bb1b63ca03818df0266
Gunther Cox <gunthercx@gmail.com>
2019-03-10 14:47:26 -0400

    Add a POS-lemma tagger for indexing

--- tests/training/test_ubuntu_corpus_training.py
+++ tests/training/test_ubuntu_corpus_training.py
@@ -176,7 +176,7 @@ class UbuntuCorpusTrainerTestCase(ChatBotTestCase):
         results = list(self.chatbot.storage.filter(text='Is anyone there?'))
 
         self.assertEqual(len(results), 2)
-        self.assertEqual(results[0].search_text, 'VBZ:anyone')
+        self.assertEqual(results[0].search_text, 'VB:anyone')
 
     def test_train_sets_search_in_response_to(self):
         """
@@ -190,7 +190,7 @@ class UbuntuCorpusTrainerTestCase(ChatBotTestCase):
         results = list(self.chatbot.storage.filter(in_response_to='Is anyone there?'))
 
         self.assertEqual(len(results), 2)
-        self.assertEqual(results[0].search_in_response_to, 'VBZ:anyone')
+        self.assertEqual(results[0].search_in_response_to, 'VB:anyone')
 
     def test_is_extracted(self):
         """
