e0c7455e99c729799ca8887bda8da3b4b10f30ca
Gunther Cox <gunthercx@gmail.com>
2019-04-10 09:50:14 -0400

    Read version from config instead of using import

--- chatterbot/__init__.py
+++ chatterbot/__init__.py
@@ -3,10 +3,6 @@ ChatterBot is a machine learning, conversational dialog engine.
 """
 from .chatterbot import ChatBot
 
-__version__ = '1.0.5'
-__author__ = 'Gunther Cox'
-__email__ = 'gunthercx@gmail.com'
-__url__ = 'https://github.com/gunthercox/ChatterBot'
 
 __all__ = (
     'ChatBot',
