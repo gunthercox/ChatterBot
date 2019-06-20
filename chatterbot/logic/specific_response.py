4073144031934635d57e76574ce0b198041a85d9
이연리 <yeonri7082@gmail.com>
2019-06-20 19:05:17 +0900

    use statement.text instead of object

--- chatterbot/logic/specific_response.py
+++ chatterbot/logic/specific_response.py
@@ -22,7 +22,7 @@ class SpecificResponseAdapter(LogicAdapter):
         self.response_statement = Statement(text=output_text)
 
     def can_process(self, statement):
-        if statement == self.input_text:
+        if statement.text == self.input_text:
             return True
 
         return False
