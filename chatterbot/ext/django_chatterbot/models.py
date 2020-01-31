93f426c11298793de0cbc71f332a1058e3cd1e99
Dominic Michael Silveira <dms24081999@gmail.com>
2020-02-01 00:28:00 +0530

    Django FIX for no such table found
    [FIXED] django.db.utils.OperationalError: no such table: django_chatterbot_statement
    [FIXED] django.db.utils.OperationalError: no such table: django_chatterbot_tag

--- chatterbot/ext/django_chatterbot/models.py
+++ chatterbot/ext/django_chatterbot/models.py
@@ -6,11 +6,13 @@ class Statement(AbstractBaseStatement):
     A statement represents a single spoken entity, sentence or
     phrase that someone can say.
     """
-    pass
+    class Meta:
+        db_table = 'statement'
 
 
 class Tag(AbstractBaseTag):
     """
     A label that categorizes a statement.
     """
-    pass
+    class Meta:
+        db_table = 'tag' 
