30a7a5b5b0ad72cd8ca2ae025875a76ac8b6f87b
Dan Caron <dancaron@gmail.com>
2021-02-01 11:16:10 -0500

    add url name

--- examples/django_app/example_app/urls.py
+++ examples/django_app/example_app/urls.py
@@ -5,6 +5,6 @@ from example_app.views import ChatterBotAppView, ChatterBotApiView
 
 urlpatterns = [
     path('', ChatterBotAppView.as_view(), name='main'),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('api/chatterbot/', ChatterBotApiView.as_view(), name='chatterbot'),
 ]
