df794b6d60aa6a50594fc8d2f4b2051ce4f579e5
Dan Caron <dancaron@gmail.com>
2021-02-01 11:14:11 -0500

    allow localhost

--- examples/django_app/example_app/settings.py
+++ examples/django_app/example_app/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'fsch+6!=q+@ol&%0x!nwdl@48^ixbd4clx5f1i!5n^66y+pmn*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
