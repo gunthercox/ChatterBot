1e721e051bb461f3a86e7822e84e77634e2089b7
bobloy <alboblexloy@gmail.com>
2021-06-29 16:00:45 -0400

    Support for all versions of 3.8

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     include_package_data=True,
     install_requires=REQUIREMENTS,
     dependency_links=DEPENDENCIES,
-    python_requires='>=3.4, <=3.8',
+    python_requires='>=3.4, <=3.8.*',
     license='BSD',
     zip_safe=True,
     platforms=['any'],
