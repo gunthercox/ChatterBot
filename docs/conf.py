aee6f58c20bd124dcaa96b30d617c207a680e1bc
Gunther Cox <gunthercx@gmail.com>
2025-01-04 16:28:17 -0500

    Remove deprecated setting

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,6 @@ pygments_style = 'sphinx'
 
 html_theme = 'sphinx_rtd_theme'
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further. For a list of options available for each theme, see the
 # documentation.
