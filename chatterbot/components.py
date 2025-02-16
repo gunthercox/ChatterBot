7598ff1e8ba88924284a63d056dbe7430642489a
Gunther Cox <gunthercx@gmail.com>
2025-02-16 14:03:37 -0500

    Move bigram logic to custom spacy component

---
+++ chatterbot/components.py
@@ -0,0 +1,52 @@
+"""
+Custom components for Spacy processing pipelines.
+https://spacy.io/usage/processing-pipelines#custom-components
+"""
+import string
+from spacy.language import Language
+from spacy.tokens import Doc
+
+
+punctuation_table = str.maketrans(dict.fromkeys(string.punctuation))
+
+
+@Language.component('chatterbot_bigram_indexer')
+def chatterbot_bigram_indexer(document):
+    """
+    Generate the text string for a bigram-based search index.
+    """
+
+    if not Doc.has_extension('bigram_index'):
+        Doc.set_extension('bigram_index', default='')
+
+    tokens = [
+        token for token in document if not (token.is_punct or token.is_stop)
+    ]
+
+    # Fall back to including stop words if needed
+    if not tokens or len(tokens) == 1:
+        tokens = [
+            token for token in document if not (token.is_punct)
+        ]
+
+    bigram_pairs = [
+        f"{tokens[i - 1].pos_}:{tokens[i].lemma_.lower()}"
+        for i in range(1, len(tokens))
+    ]
+
+    if not bigram_pairs:
+
+        text_without_punctuation = document.text.translate(
+            punctuation_table
+        )
+        if len(text_without_punctuation) >= 1:
+            text = text_without_punctuation.lower()
+        else:
+            text = document.text.lower()
+
+        bigram_pairs = [text]
+
+    # Assign a custom attribute at the Doc level
+    document._.bigram_index = ' '.join(bigram_pairs)
+
+    return document
