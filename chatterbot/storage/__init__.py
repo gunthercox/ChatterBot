03ab50876305b5ad33df197cb85564a222c8b3b7
Gunther Cox <gunthercx@gmail.com>
2025-03-04 08:06:32 -0500

    Documentation updates

--- chatterbot/storage/__init__.py
+++ chatterbot/storage/__init__.py
@@ -2,6 +2,7 @@ from chatterbot.storage.storage_adapter import StorageAdapter
 from chatterbot.storage.django_storage import DjangoStorageAdapter
 from chatterbot.storage.mongodb import MongoDatabaseAdapter
 from chatterbot.storage.sql_storage import SQLStorageAdapter
+from chatterbot.storage.redis import RedisVectorStorageAdapter
 
 
 __all__ = (
@@ -9,4 +10,5 @@ __all__ = (
     'DjangoStorageAdapter',
     'MongoDatabaseAdapter',
     'SQLStorageAdapter',
+    'RedisVectorStorageAdapter',
 )
