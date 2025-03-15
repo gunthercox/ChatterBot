387a6892a1037d6060ecd775a53ea129366bf283
Gunther Cox <gunthercx@gmail.com>
2025-03-15 10:03:09 -0400

    Add --help fallback command

--- chatterbot/__main__.py
+++ chatterbot/__main__.py
@@ -1,7 +1,16 @@
+"""
+Example usage for ChatterBot command line arguments:
+
+python -m chatterbot --help
+"""
+
 import sys
 
 
 def get_chatterbot_version():
+    """
+    Return the version of the current package.
+    """
     from chatterbot import __version__
 
     return __version__
@@ -10,3 +19,9 @@ def get_chatterbot_version():
 if __name__ == '__main__':
     if '--version' in sys.argv:
         print(get_chatterbot_version())
+    elif '--help' in sys.argv:
+        print('usage: chatterbot [--version, --help]')
+        print('  --version: Print the version of ChatterBot')
+        print('  --help: Print this help message')
+        print()
+        print('Documentation at https://docs.chatterbot.us')
--- docs/commands.rst
+++ docs/commands.rst
@@ -3,6 +3,7 @@ Command line tools
 ==================
 
 ChatterBot comes with a few command line tools that can help
+with general packaging-related tasks.
 
 Get the installed ChatterBot version
 ====================================
@@ -13,3 +14,13 @@ you have then you can run the following command.
 .. code-block:: bash
 
    python -m chatterbot --version
+
+
+List available commands
+=======================
+
+To see a list of all available commands you can run the following:
+
+.. code-block:: bash
+
+   python -m chatterbot --help
