23d7a41139db3e4cd34f99a7e8985cbb474c4b7f
Gunther Cox <gunthercx@gmail.com>
2025-03-30 15:23:45 -0400

    Fix setting setup

--- chatterbot/ext/django_chatterbot/apps.py
+++ chatterbot/ext/django_chatterbot/apps.py
@@ -6,3 +6,10 @@ class DjangoChatterBotConfig(AppConfig):
     name = 'chatterbot.ext.django_chatterbot'
     label = 'django_chatterbot'
     verbose_name = 'Django ChatterBot'
+
+    def ready(self):
+        from chatterbot.ext.django_chatterbot import settings as defaults
+        from django.conf import settings
+
+        settings.CHATTERBOT = getattr(settings, 'CHATTERBOT', {})
+        settings.CHATTERBOT.update(defaults.CHATTERBOT_DEFAULTS)
