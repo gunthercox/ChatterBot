c259a7f49a2c9ed00df7804f2123c7c6211a1983
Gunther Cox <gunthercx@gmail.com>
2025-05-20 07:56:52 -0400

    Remove constant for max length

--- chatterbot/constants.py
+++ chatterbot/constants.py
@@ -4,17 +4,6 @@ ChatterBot constants
 from chatterbot import languages
 
 '''
-The maximum length of characters that the text of a statement can contain.
-The number 255 is used because that is the maximum length of a char field
-in most databases. This value should be enforced on a per-model basis by
-the data model for each storage adapter.
-
-DEPRECATED: This value is no longer as of ChatterBot 1.2.7. Statement tables
-are now created with a text field.
-'''
-STATEMENT_TEXT_MAX_LENGTH = 255
-
-'''
 The maximum length of characters that the text label of a conversation can contain.
 The number 32 was chosen because that is the length of the string representation
 of a UUID4 with no hyphens.
