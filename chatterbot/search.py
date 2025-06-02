7d9f41f730c5b2facf6a9744d31649dbf3d003fd
Anas <153397401+annuaicoder@users.noreply.github.com>
2025-06-01 21:23:51 -0700

    Improve search algorithms

--- chatterbot/search.py
+++ chatterbot/search.py
@@ -1,47 +1,64 @@
-class IndexedTextSearch:
-    """
-    :param statement_comparison_function: A comparison class.
-        Defaults to ``LevenshteinDistance``.
+from chatterbot.comparisons import LevenshteinDistance, SpacySimilarity
 
-    :param search_page_size:
-        The maximum number of records to load into memory at a time when searching.
-        Defaults to 1000
+
+class BaseTextSearch:
+    """
+    Base class for performing text search using ChatterBot comparison functions.
+    """
+
+    def __init__(self, chatbot, **kwargs):
+        self.chatbot = chatbot
+
+        # Use a better semantic comparator if available (SpacySimilarity is more contextual)
+        comparison_class = kwargs.get('statement_comparison_function', SpacySimilarity)
+
+        self.compare_statements = comparison_class(language=self.chatbot.tagger.language)
+
+        # Max results returned and how many records to load from storage at once
+        self.search_page_size = kwargs.get('search_page_size', 1000)
+        self.max_results = kwargs.get('max_results', 5)
+
+    def _search_statements(self, input_statement, filter_parameters):
+        """
+        Shared internal method to search for similar statements.
+        :param input_statement: The input statement to compare against known responses.
+        :param filter_parameters: Filter arguments for querying the storage.
+        :return: List of top-N similar statements with confidence scores.
+        """
+        self.chatbot.logger.info('Fetching candidate statements from storage...')
+        candidates = self.chatbot.storage.filter(**filter_parameters)
+
+        results = []
+
+        for candidate in candidates:
+            # Compare input statement to the 'in_response_to' field
+            comparison_text = candidate.in_response_to or candidate.text
+
+            confidence = self.compare_statements.compare_text(
+                input_statement.text,
+                comparison_text
+            )
+
+            # Store the confidence in the statement object
+            candidate.confidence = confidence
+            results.append(candidate)
+
+        # Sort results by descending confidence
+        results.sort(key=lambda stmt: stmt.confidence, reverse=True)
+
+        # Limit to top-N best results
+        return results[:self.max_results]
+
+
+class IndexedTextSearch(BaseTextSearch):
+    """
+    Indexed search that restricts candidates to those where input matches part of 'in_response_to'.
     """
 
     name = 'indexed_text_search'
 
-    def __init__(self, chatbot, **kwargs):
-        from chatterbot.comparisons import LevenshteinDistance
-
-        self.chatbot = chatbot
-
-        statement_comparison_function = kwargs.get(
-            'statement_comparison_function',
-            LevenshteinDistance
-        )
-
-        self.compare_statements = statement_comparison_function(
-            language=self.chatbot.tagger.language
-        )
-
-        self.search_page_size = kwargs.get(
-            'search_page_size', 1000
-        )
-
     def search(self, input_statement, **additional_parameters):
-        """
-        Search for close matches to the input. Confidence scores for
-        subsequent results will order of increasing value.
-
-        :param input_statement: A statement.
-        :type input_statement: chatterbot.conversation.Statement
-
-        :param **additional_parameters: Additional parameters to be passed
-            to the ``filter`` method of the storage adapter when searching.
-
-        :rtype: Generator yielding one closest matching statement at a time.
-        """
-        self.chatbot.logger.info('Beginning search for close text match')
+        self.chatbot.logger.info('Performing indexed text search...')
 
         search_parameters = {
             'search_in_response_to_contains': input_statement.search_text,
@@ -52,73 +69,18 @@ class IndexedTextSearch:
         if additional_parameters:
             search_parameters.update(additional_parameters)
 
-        statement_list = self.chatbot.storage.filter(**search_parameters)
-
-        best_confidence_so_far = 0
-
-        self.chatbot.logger.info('Processing search results')
-
-        # Find the closest matching known statement
-        for statement in statement_list:
-            confidence = self.compare_statements.compare_text(
-                input_statement.text, statement.in_response_to
-            )
-
-            if confidence > best_confidence_so_far:
-                best_confidence_so_far = confidence
-                statement.confidence = confidence
-
-                self.chatbot.logger.info('Similar text found: {} {}'.format(
-                    statement.in_response_to, confidence
-                ))
-
-                yield statement
+        return self._search_statements(input_statement, search_parameters)
 
 
-class TextSearch:
+class TextSearch(BaseTextSearch):
     """
-    :param statement_comparison_function: A comparison class.
-        Defaults to ``LevenshteinDistance``.
-
-    :param search_page_size:
-        The maximum number of records to load into memory at a time when searching.
-        Defaults to 1000
+    General search that compares input with all known responses.
     """
 
     name = 'text_search'
 
-    def __init__(self, chatbot, **kwargs):
-        from chatterbot.comparisons import LevenshteinDistance
-
-        self.chatbot = chatbot
-
-        statement_comparison_function = kwargs.get(
-            'statement_comparison_function',
-            LevenshteinDistance
-        )
-
-        self.compare_statements = statement_comparison_function(
-            language=self.chatbot.tagger.language
-        )
-
-        self.search_page_size = kwargs.get(
-            'search_page_size', 1000
-        )
-
     def search(self, input_statement, **additional_parameters):
-        """
-        Search for close matches to the input. Confidence scores for
-        subsequent results will order of increasing value.
-
-        :param input_statement: A statement.
-        :type input_statement: chatterbot.conversation.Statement
-
-        :param **additional_parameters: Additional parameters to be passed
-            to the ``filter`` method of the storage adapter when searching.
-
-        :rtype: Generator yielding one closest matching statement at a time.
-        """
-        self.chatbot.logger.info('Beginning search for close text match')
+        self.chatbot.logger.info('Performing general text search...')
 
         search_parameters = {
             'persona_not_startswith': 'bot:',
@@ -128,24 +90,4 @@ class TextSearch:
         if additional_parameters:
             search_parameters.update(additional_parameters)
 
-        statement_list = self.chatbot.storage.filter(**search_parameters)
-
-        best_confidence_so_far = 0
-
-        self.chatbot.logger.info('Processing search results')
-
-        # Find the closest matching known statement
-        for statement in statement_list:
-            confidence = self.compare_statements.compare_text(
-                input_statement.text, statement.in_response_to
-            )
-
-            if confidence > best_confidence_so_far:
-                best_confidence_so_far = confidence
-                statement.confidence = confidence
-
-                self.chatbot.logger.info('Similar text found: {} {}'.format(
-                    statement.text, confidence
-                ))
-
-                yield statement
+        return self._search_statements(input_statement, search_parameters)
